4fccffb97fc8162302c782dcb895b4583b7c90d3
Victoria Quennessen <viquennessen@gmail.com>
2019-07-21 08:54:22 -0600

    fixed catch_at_age function

--- code/R/catch_at_age.R
+++ code/R/catch_at_age.R
@@ -1,5 +1,14 @@
+#' Based on the continuous (Baranov) formulation of of selectivity, i.e. that
+#' fishing mortality (FM) and natural mortality (M) are proportional to fish 
+#' abundance (N) and act simultaneously  and uniformly throughout the year, 
+#' i.e. dN/dt = -(M+F)*N
+
 catch_at_age <- function(a, t, cr, N, FM) {
   
-  catch[ , a, t, cr] <- N[ , a, t, cr]*FM[ , a, t, cr]
+  cf <- FM[ , a, t, cr]/(M + FM[ , a, t, cr])
+  
+  catch[ , a, t, cr] <- cf * N[ , a, t, cr]*exp(-1*M - FM[ , a, t, cr])
+  
+  return(catch)
   
 }
