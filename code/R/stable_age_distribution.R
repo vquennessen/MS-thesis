bcdb833751a9103c160f2a3e9380bdc332286fbb
Victoria Quennessen <viquennessen@gmail.com>
2019-08-13 15:02:45 +0200

    fixed indices and comments

--- code/R/stable_age_distribution.R
+++ code/R/stable_age_distribution.R
@@ -13,31 +13,36 @@ stable_age_distribution <- function(b, c, max_age, m, L0, W0, rec_age, M, Fb,
   L1 <- diag(Y*Z, length(Y))
   L2 <- fecundity*(ages >= m)
   
-  # Multiply fecundity by "a" value, as outlined in Hilborn paper
-  # Q1 as outlined in Lawson & Hilborn 1985
+  ### Multiply fecundity by "a" value, as outlined in Hilborn paper
+  
+  # B is the inverse of the asymptotic recruitment in the Beverton-Holt
+  # stock recruitment curve
   B <- (h - 0.2) / (0.8 * h * R0)
   
+  # Q1 as outlined in Lawson & Hilborn 1985
   sum1 <- 0
   for (i in rec_age:n) {
-    sum1 <- sum1 + W[i]*exp(-M * (i - 1 - rec_age))
+    sum1 <- sum1 + W[i - rec_age + 1]*exp(-M * (i - rec_age))
   }
   
   Q1 <- W[1] + sum1
   
   E0 <- R0 * Q1
   
+  # A is the inverse of the initial slope of the Beverton-Holt stock 
+  # recruitment curve 
   A <- Q1 - B*E0
   
   L2 <- L2*A
   
-  # Put matrix together
+  ### Put matrix together
   one <- two <- matrix(rep(0, length(ages)^2), nrow = length(ages))
   one[2:nrow(one), 1:(ncol(one) - 1)] <- L1
   two[1, ] <- L2
   
   LM <- one + two
 
-  # pull out eigenvectors (V) and eigenvalues (L) of A0
+  ### pull out eigenvectors (V) and eigenvalues (L) of A0
   e <- eigen(LM)
   V <- Re(e$vectors)
   L <- Re(e$values)
