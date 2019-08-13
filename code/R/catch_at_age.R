29e28d56f41363e522c2f91784bccfa40bce3e8f
Victoria Quennessen <viquennessen@gmail.com>
2019-08-13 15:33:58 +0200

    fixed discrete catch formulation equation (I think)

--- code/R/catch_at_age.R
+++ code/R/catch_at_age.R
@@ -1,6 +1,6 @@
 catch_at_age <- function(a, t, cr, N, FM, catch, catch_form, season) {
   
-  # if (catch_formulation == 'continuous') {
+  if (catch_form == 'continuous') {  
     
     #' Based on the continuous (Baranov) formulation of of selectivity, i.e. that
     #' fishing mortality (FM) and natural mortality (M) are proportional to fish 
@@ -10,21 +10,23 @@ catch_at_age <- function(a, t, cr, N, FM, catch, catch_form, season) {
     coeff <- FM[ , a, t, cr]/(M + FM[ , a, t, cr])
     
     catch[ , a, t, cr] <- coeff * N[ , a, t, cr] * exp(-1*(M + FM[ , a, t, cr]))
-  #'   
-  #' } else if (catch_formulation == 'discrete') {
-  #'   
-  #'   #' Based on the discrete formulation of selectivity, which assumes that 
-  #'   #' fishing occurs in a very short pulse and no natural mortality occurs
-  #'   #' during the fishing season, which could be at the start, end, or any 
-  #'   #' other part of the year, determined by the variable fishing, which is
-  #'   #' 0 for at the beginning of the year, 1 for the end, and on the interval
-  #'   #' (0, 1) for anywhere in between
-  #'   
-  #'   u <- 1 - exp(FM[, a, t, cr]) 
-  #'   
-  #'   catch[, a, t, cr] <- N[, a, t, cr]*S*u*exp(-1*(season - t)*M)
-  #'   
-  #' }
+    
+  } else if (catch_form == 'discrete') {
+    
+    #' Based on the discrete formulation of selectivity, which assumes that
+    #' fishing occurs in a very short pulse and no natural mortality occurs
+    #' during the fishing season, which could be at the start, end, or any
+    #' other part of the year, determined by the variable fishing, which is
+    #' 0 for at the beginning of the year, 1 for the end, and on the interval
+    #' (0, 1) for anywhere in between
+    
+    vulnerability <- vulnerability_to_gear(a, t, cr, A, Fb, E)
+    
+    u <- 1 - exp(-1*vulnerability*E[a, t, cr])
+    
+    catch[, a, t, cr] <- N[, a, t, cr]*S*u*exp(-1*(season - t)*M)
+    
+  }
   
   return(catch[, a, t, cr])
   
