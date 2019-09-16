98738a4afb233686c25152cecb535e731d86446c
Victoria Quennessen <viquennessen@gmail.com>
2019-09-16 09:11:30 -0700

    "" --> '' (keeping formatting consistent throughout functions) and changed p --> AMP for adult movement proportion

--- code/R/parameters.R
+++ code/R/parameters.R
@@ -13,7 +13,7 @@ parameters = function(species) {
     stop("Study species must be a character string.")
   }
   
-  if (species == "black rockfish 2003") {
+  if (species == 'black rockfish 2003') {
     # source: Babcock & MacCall 2011
     max_age <- 35                           # maximum age
     M <- 0.14                               # natural mortality
@@ -31,7 +31,7 @@ parameters = function(species) {
     phi <- 1.1                              # unfished recruits per spawner
     sigma_R <- 0.5                          # recruitment standard deviation
     rho_R <- 0                              # recruitment autocorrelation
-    p <- 0.1                                # adult movement proportion
+    AMP <- 0.1                              # adult movement proportion
     D <- 0.488                              # depletion
     Fb <- 0.2                               # fishing mortality to cause D
     r <- 0.77                               # Proportion of positive transects 
@@ -61,10 +61,58 @@ parameters = function(species) {
                                             
   }
   
+  if (species == 'black rockfish 2015') {
+    # source: Cope et al. 2016
+    max_age <- 40                           # maximum age
+    M <- 0.17                               # natural mortality
+    rec_age <- 3                            # age at recruitment
+    af <- 2.6e-5; bf <- 2.88                # weight at length parameters (f)
+    am <- 2.58e-5; bm <- 2.89               # weight at length parameters (m)
+    a1f <- 1; L1f <- 20.32;                 # growth parameters (f)
+    a2f <- 40; L2f <- 49.67; Kf <- 0.21  
+    a1m <- 1; L1m <- 17.47;                 # growth parameters (m)
+    a2m <- 40; L2m <- 43.27; Km <- 0.34  
+    L50 <- 43.69                            # length at 50% maturity
+    k_mat <- -0.66                          # slope of maturity curve
+    ldp <- 0.1                              # larval drift proportion
+    h <- 0.77                               # steepness
+    phi <- 1.1                              # unfished recruits per spawner
+    sigma_R <- 0.5                          # recruitment standard deviation
+    rho_R <- 0                              # recruitment autocorrelation
+    AMP <- 0.1                              # adult movement proportion
+    D <- 0.604                              # depletion
+    Fb <- XXX                               # fishing mortality to cause D
+    r <- 0.77                               # Proportion of positive transects 
+    #       in PISCO monitoring data
+    x <- 15.42                              # mean of positive transects
+    sp <- 16.97                             # std of positive transects
+    
+    c <- 0.2747                             # eggs produced per g, intercept
+    b <- 0.0941                             # eggs produced per g, slope
+    
+    #### fleets: sport, hook, trawl ####
+    fleets <- c('sport', 'hook', 'trawl')   # names of fleets
+    alpha <- c(0.45, 0.369, 0.426)          # slope of upcurve per fleet
+    beta <- c(1.28, 0.419, 0)               # slope of downcurve per fleet
+    start <- c(20, 20, 20)                  # vulnerability start length / fleet
+    F_fin <- c(0.265, 0.29, 0)              # final selectivity if dome-shaped 
+    L50_up <- c(25.2, 33.2, 46)             # L50 value for upcurve
+    L50_down <- c(36.7, 46.4, 50)           # L50 value for downcurve
+    cf <- c(0.71, 0.28, 0.01)               # fraction of fishery caught / fleet
+    switch <- c(29, 29, 0)                  # length where selectivity switches 
+    #       from upcurve to 1
+    full <- c(31, 39, 0)                    # length at which downcurve starts
+    catch_form <- 'continuous'              # discrete or continuous catch
+    season <- 0.5                           # if catch_formulation = discrete, 
+    #       time at which fishing occurs
+    #       0 = beginning, 1 = end
+    
+  }
+  
   output = list(max_age, M, rec_age, af, bf, am, bm, a1f, L1f, a2f, L2f, Kf,
                 a1m, L1m, a2m, L2m, Km, L50, k_mat, ldp, h, phi, sigma_R,
-                rho_R, p, D, Fb, r, x, sp, c, b, fleets, alpha, beta, start, 
-                F_fin, L50_up, L50_down, cf, switch, full, catch_form, fishing)
+                rho_R, AMP, D, Fb, r, x, sp, c, b, fleets, alpha, beta, start, 
+                F_fin, L50_up, L50_down, cf, switch, full, catch_form, season)
   
   return(output)
   
