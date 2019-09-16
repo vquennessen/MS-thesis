ab3561a03b99c3b0cd3ba3fd1da50d8b1087d22a
Victoria Quennessen <viquennessen@gmail.com>
2019-09-16 09:09:50 -0700

    eliminated extraneous variable in epsilon function

--- code/R/epsilon.R
+++ code/R/epsilon.R
@@ -8,27 +8,22 @@
 #' @export
 #'
 #' @examples
-#' t <- 50 
-#' par <- parameters("black rockfish")
-#' sigma_R <- par[[24]]
-#' rho_R <- par[[25]]
-#' epsilon(t, sigma_R, rho_R)
 
-epsilon <- function (A, time, CR, nuR, sigma_R, rho_R) {
+epsilon <- function (A, timeT, CR, nuR, rho_R) {
   
   # Error term for recruitment
   # Based on Babcock & MacCall (2011): Eq. (4)
   
   # initialize epsilon vector
-  # Dimensions = area * time * CR
-  Eps <- array(rep(0, A*time*CR), c(A, time, CR))
+  # Dimensions = area * timeT * CR
+  Eps <- array(rep(0, A*timeT*CR), c(A, timeT, CR))
   
   # eps[, 1, ]
   Eps[, 1, ] <- nuR[, 1, ]*sqrt(1 + rho_R^2)
   
   # fill in rest of epsilon vector
   for (a in 1:A) {
-    for (t in 2:time) {
+    for (t in 2:timeT) {
       for (cr in 1:CR) {
         Eps[a, t, cr] <- rho_R*Eps[a, t-1, cr] + nuR[a, t, cr]*sqrt(1 + rho_R^2)
       }
