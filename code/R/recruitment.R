095a30117328e46f03517e82962d1fca6281aa62
Victoria Quennessen <viquennessen@gmail.com>
2019-09-16 09:12:41 -0700

    got rid of extraneous lines in recruitment function

--- code/R/recruitment.R
+++ code/R/recruitment.R
@@ -13,21 +13,6 @@
 #' @export
 #'
 #' @examples
-#' par <- parameters("black rockfish")
-#' max_age <- par[[1]]
-#' a1f <- par[[8]]; L1f <- par[[9]]; a2f <- par[[10]]; L2f <- par[[11]]; 
-#' Kf <- par[[12]]; af <- par[[4]]; bf <- par[[5]]
-#' L50 <- par[[18]]; k_mat <- par[[19]]
-#' R0 <- par[[21]]; h <- par[[22]]; sigma_R <- par[[24]]; B0 <- par[[32]] 
-#' 
-#' A <- 5
-#' 
-#' epsilon <- epsilon(1, sigma_R, rho_R)
-#' N <- array(rep(100, max_age), c(1, max_age))
-#' L <- length_at_age(max_age, L1f, L2f, Kf, a1f, a2f)
-#' W <- weight_at_age(L, af, bf)
-#' M <- fraction_mature_at_age(max_age, k_mat, L, L50)
-#' B <- spawning_stock_biomass(N, W, M)
 
 recruitment = function(a, t, cr, SSB, A, R0, h, B0, Eps, sigma_R) {
   
