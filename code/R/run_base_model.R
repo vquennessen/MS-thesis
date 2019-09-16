39856292c2b9a87a1858da6b5fb9d4bb83d49295
Victoria Quennessen <viquennessen@gmail.com>
2019-09-16 09:13:02 -0700

    wrote script to run base_model given parameters

---
+++ code/R/run_base_model.R
@@ -0,0 +1,16 @@
+source('./code/R/base_model.R')
+
+species <- 'black rockfish 2003'
+A <- 5
+time1 <- 50
+time2 <- 50
+allocation <- 'equal'
+R0 <- 1e+5
+stochasticity <- T
+surveys <- T
+fishery_management <- T
+fishing <- T
+adult_movement <- T
+
+base_model(species, A, time1, time2, allocation, R0, stochasticity, surveys, 
+           fishery_management, fishing, adult_movement)
