8c8907535e74387d35abc089e9a5ce0b5127d173
Victoria Quennessen <viquennessen@gmail.com>
2019-09-17 23:11:44 -0700

    switched from using catch to yield for IFD effort allocation

--- code/R/effort_allocation.R
+++ code/R/effort_allocation.R
@@ -1,19 +1,44 @@
-effort_allocation <- function(a, t, cr, allocation, A, E, biomass, time1) {
+effort_allocation <- function(a, t, cr, allocation, A, E, yield, time1) {
   
+  # separate areas inside and outside of reserve
+  areas <- 1:A
+  reserve <- median(areas)
+  outside <- areas[-reserve]
+  
+  #' If effort is allocated using the ideal free distribution, effort for one 
+  #' year depends on the distribution of yield from the previous year
   if (allocation == 'IFD') {
     
-    prop_biomass <- biomass[, t - 1]/sum(biomass[, t - 1, cr])
-    E[a, t, cr] <- sum(E[, t - 1, cr])*prop_biomass
+    # Before reserve implementation
+    if (t < time1) {
+      
+      prop_yield <- yield[, t - 1, cr]/sum(yield[, t - 1, cr])
+      E[a, t, cr] <- sum(E[, t - 1, cr])*prop_yield
+      
+      #' After reserve implementation, effort in the reserve is equal to zero
+    } else if (t >= time1) {
+      
+      prop_yield_outside <- yield[outside, t - 1]/sum(yield[outside, t - 1, cr])
+      
+      E[outside, t, cr] <- sum(E[, t - 1, cr])*prop_yield_outside
+      E[reserve, t, cr] <- 0
+      
+    }
     
-  } else if (t >= time1) {
+  #' Otherwise, distribute effort equally between the four areas outside the 
+  #' marine reserve, regardless of yield
+  } else if (allocation == 'equal') {
     
-    areas <- 1:A
-    reserve <- median(areas)
-    outside <- areas[-reserve]
+    if (time < time1) {
+      
+      E[, t, cr] <- rep(sum(E[, t, cr])/A, A)
+      
+    } else if (time >= time1) {
     
     E[reserve, t, cr] <- 0
+    E[outside, t, cr] <- rep(sum(E[, t-1, cr])/(A-1), A-1)
     
-    E[outside, t, cr] <- rep(sum(E[, t - 1, cr])/(A-1), A - 1)
+    }
     
   }
   
