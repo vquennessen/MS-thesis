f558f0d09fda366dd6efc3951da9b40df7dcb730
Victoria Quennessen <viquennessen@gmail.com>
2019-09-17 23:10:26 -0700

    added yield2 array and rho_R variable and updated SSB function to use rec_age

--- code/R/equilibrium_SAD.R
+++ code/R/equilibrium_SAD.R
@@ -1,6 +1,6 @@
 equilibrium_SAD <- function(a, cr, allocation, A, rec_age, max_age, n, W, R0,
-                            Mat, h, B0, Eps, sigma_R, Fb, S, M, season,
-                            catch_form, eq_time, m, stochasticity) {
+                            Mat, H, B0, Eps, sigma_R, Fb, S, M, season,
+                            catch_form, eq_time, m, stochasticity, rho_R) {
   
   # Initialize population size and catch arrays
   # Dimensions = age * 1 * time * 1
@@ -8,7 +8,7 @@ equilibrium_SAD <- function(a, cr, allocation, A, rec_age, max_age, n, W, R0,
   
   # Initialize biomass and SSB arrays
   # Dimensions = 1 * time * 1
-  biomass2 <- SSB2 <- E2 <- array(rep(0, eq_time), c(1, eq_time, 1))
+  biomass2 <- yield2 <- SSB2 <- E2 <- array(rep(0, eq_time), c(1, eq_time, 1))
   abundance_all2 <- abundance_mature2 <- array(rep(0, eq_time), c(1, eq_time, 1))
   
   # Recruitment normal variable
@@ -33,15 +33,17 @@ equilibrium_SAD <- function(a, cr, allocation, A, rec_age, max_age, n, W, R0,
     FM2[, 1, t, 1] <- fishing_mortality(1, t, 1, FM2, A, Fb, E2, S)
     N2[, 1, t, 1] <- rep(10, n)
     biomass2[1, t, 1] <- sum(N2[, 1, t, 1] * W)
+    SSB2[1, t, 1] <- spawning_stock_biomass(1, t, 1, rec_age, N2, W, Mat)
     catch2[, 1, t, 1] <- catch_at_age(1, t, 1, FM2, M, N2, A, Fb, E2, catch2, 
                                       catch_form, season)
+    yield2[1, t, 1] <- sum(catch2[, a, t, cr]*W)
   }
 
   # Step population forward in time with set fishing level
   for (t in (rec_age + 1):(eq_time - 1)) {
     
     # effort allocation
-    E2 <- effort_allocation(1, t, 1, allocation, A, E2, biomass2, eq_time)
+    E2 <- effort_allocation(1, t, 1, allocation, A, E2, yield2, eq_time)
     
     # biology
     PD <- pop_dynamics(1, t, 1, rec_age, max_age, n, SSB2, N2, W, Mat, A, R0, 
@@ -57,6 +59,7 @@ equilibrium_SAD <- function(a, cr, allocation, A, rec_age, max_age, n, W, R0,
     catch2[, 1, t, 1] <- catch_at_age(1, t, 1, FM2, M, N2, A, Fb, E2, catch2, 
                                      catch_form, season)
     N2[, 1, t, 1] <- N2[, 1, t, 1] - catch2[, 1, t, 1]
+    yield2[1, t, 1] <- sum(catch2[, 1, t, 1]*W)
     
   }
   
