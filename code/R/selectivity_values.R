f30c1e5dd8d775e765dc01a4015357641c4019e7
Victoria Quennessen <viquennessen@gmail.com>
2019-10-03 11:07:37 -0700

    finished finding selectivity parameters! Woot!

--- code/R/selectivity_values.R
+++ code/R/selectivity_values.R
@@ -120,21 +120,33 @@ L <- length_at_age(age, L1f, L2f, Kf, a1f, a2f) # length at age
 fleet <- c('trawl', 'live', 'dead', 'recO', 'recS')
 L50upA <- c(7, 5, 5, 5, 3)
 L50upL <- L[L50upA - rec_age + 1]
-alpha <- c(0.8473, 0.6931, 2.1972, 1.3863, 2.1972)
-Ffin <- c(0.6, 0.275, 0.2, 0.015, 0.13)
-L50downA <- c(15, 13, 14, 12, 6.5)
+alpha <- c(0.325, 0.4, 0.35, 0.65, 0.425)
+Ffin <- c(0.325, 0.05, -0.11, -0.025, 0.135)
+L50downA <- c(15, 13, 13, 12, 6.5)
 L50downL <- L[L50downA - rec_age + 1]
-beta <- c(-0.1703, 0.5229, 0.6486, 0.5593, 0.1231)
+beta <- c(0.25, 0.5, 0.4, 1.1, 0.5)
 
 # initialize upcurves and downcurves
 f <- length(fleet)
 l <- length(L)
-upcurve <- array(rep(NA, f * l), c(f, l))
-downcurve <- array(rep(NA, f * l), c(f, l))
-
-for (i in 1:l) {
-  upcurve[i, ] <- 1 / (1 + exp(-1 * alpha[i] * (L - L50upL[i])))
-  downcurve[i, ] <- 1 - (1 - Ffin[i]) / (1 + exp(-1 * beta[i] * (L - L50downL[i])))
+upcurve <- array(rep(NA, f * (max_age + 1)), c(f, max_age + 1))
+downcurve <- array(rep(NA, f * (max_age + 1)), c(f, max_age + 1))
+for (i in 1:rec_age) {
+  upcurve[, i] <- downcurve[, i] <- 0
 }
 
-plot(age, upcurve[1, ], type = 'l', lwd = 2, col = 'darkblue')
+for (i in 1:f) {
+  upcurve[i, age + 1] <- 1 / (1 + exp(-1 * alpha[i] * (L - L50upL[i])))
+  downcurve[i, age + 1] <- 1 - (1 - Ffin[i]) / (1 + exp(-1 * beta[i] * (L - L50downL[i])))
+}
+
+x <- 5
+plot(0:max_age, upcurve[x, ], type = 'l', lwd = 2, col = 'red', 
+     ylim = c(0, 1))
+lines(0:max_age, downcurve[x, ], lwd = 2, col = 'red')
+abline(v = 4.5, col = 'red')
+abline(h = 0.875, col = 'red')
+abline(h = 0.14, col = 'green')
+abline(h = 0.5, col = 'blue')
+abline(v = 3, col = 'blue')
+abline(v = 6.25, col = 'blue')
