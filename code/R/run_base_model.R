94dc977b3915dc6e2ce90563bba1cf607aa593b8
Victoria Quennessen <viquennessen@gmail.com>
2019-10-24 15:18:03 -0700

    adding write results to file to save results from multiple simulations
    adding timing to time how long it takes to run different numbers of simulations

--- code/R/run_base_model.R
+++ code/R/run_base_model.R
@@ -6,13 +6,56 @@ species <- 'black rockfish 2015'
 A <- 5
 time1 <- 50
 time2 <- 50
+CR <- 8
 allocation <- 'IFD'
 R0 <- 1e+5
 stochasticity <- T
 surveys <- T
+transects <- 24
 fishery_management <- T
 fishing <- T
 adult_movement <- T
 
-base_model(species, A, time1, time2, allocation, R0, stochasticity, surveys, 
-           fishery_management, fishing, adult_movement)
+# set numbers of simulations
+sims <- c(1, 2, 10, 100)
+
+data <- as.data.frame(sims)
+data$time <- rep(0, length(sims))
+
+for (j in 1:length(sims)) {
+  
+  start <- Sys.time()
+  
+  num_sims <- sims[j]
+  
+  # initialize yield and biomass arrays
+  sims_yield <- array(rep(0, A*time2*CR*num_sims), c(A, time2, CR, num_sims))
+  sims_biomass <- array(rep(0, A*time2*CR*num_sims), c(A, time2, CR, num_sims))
+  
+  # run the model for each simulation
+  for (i in 1:num_sims) {
+    
+    output <- base_model(species, A, time1, time2, CR, allocation, R0, 
+                         stochasticity, surveys, transects, fishery_management, 
+                         fishing, adult_movement)
+    
+    # save the relative yield and biomasses for all areas, times after reserve 
+    # implementation, and control rules
+    sims_yield[, , , i] <- output[[1]]
+    sims_biomass[, , , i] <- output[[2]]
+    
+  }
+  
+  # write the simulation results to .csv files
+  write.table(sims_yield, row.names = F, col.names = F, append = F,
+              file = "C:/Users/Vic/Documents/Projects/DensityRatio/data/testing_yield.csv")
+  
+  write.table(sims_biomass, row.names = F, col.names = F, append = F,
+              file = "C:/Users/Vic/Documents/Projects/DensityRatio/data/testing_biomass.csv")
+  
+  end <- Sys.time()
+  
+  data$time[j] <- difftime(end, start, units = 'mins')
+}
+
+data
