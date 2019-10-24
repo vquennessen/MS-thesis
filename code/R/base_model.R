15f3e0d65697e81ab6452ee9e8da56871f2eb7f7
Victoria Quennessen <viquennessen@gmail.com>
2019-10-23 22:00:03 -0700

    playing around with figure y-axes

--- code/R/base_model.R
+++ code/R/base_model.R
@@ -237,8 +237,8 @@ base_model <- function(species, A, time1, time2, allocation, R0, stochasticity,
   par(mar = c(5.1, 4.1, 4.1, 8.7), xpd = T)
   
   # y-axis limits
-  y1 <- 0
-  y2 <- 2
+  y1 <- 0.5
+  y2 <- 3.5
   y_by <- (y2 - y1)/2
   
   # x-axis limits
@@ -306,8 +306,8 @@ base_model <- function(species, A, time1, time2, allocation, R0, stochasticity,
   }
   
   # y-axis limits
-  yy1 <- -1
-  yy2 <- 1 
+  yy1 <- 0
+  yy2 <- 6 
   yy_by <- (yy2 - yy1)/2
   
   # x-axis limits
