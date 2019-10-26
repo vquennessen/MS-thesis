7b9973aae35e7dc3eecb0cb83fbe53a1b513fb3e
Victoria Quennessen <viquennessen@gmail.com>
2019-10-26 15:37:13 -0700

    added plot_stuff() function to run_base_model() function; decreased number of timesteps; stopped plotting things in regular base_model()

--- code/R/run_base_model.R
+++ code/R/run_base_model.R
@@ -1,14 +1,17 @@
 rm(list = ls())
+cat("\014")
+dev.off()
 
 setwd("C:/Users/Vic/Documents/Projects/DensityRatio/code/R")
 
 source('./base_model.R')
+source('./plot_stuff.R')
 
 species <- 'black rockfish 2003'
 A <- 5
-time1 <- 50
-time2 <- 50
-CR <- 7
+time1 <- 20
+time2 <- 20
+CR <- 5
 allocation <- 'IFD'
 R0 <- 1e+5
 stochasticity <- T
@@ -17,9 +20,10 @@ transects <- 24
 fishery_management <- T
 fishing <- T
 adult_movement <- T
+plotting <- F
 
 # set numbers of simulations
-num_sims <- 100
+num_sims <- 50
 
 # initialize yield and biomass arrays
 sims_yield <- array(rep(0, A*time2*CR*num_sims), c(A, time2, CR, num_sims))
@@ -30,7 +34,7 @@ for (i in 1:num_sims) {
   
   output <- base_model(species, A, time1, time2, CR, allocation, R0, 
              stochasticity, surveys, transects, fishery_management, 
-             fishing, adult_movement)
+             fishing, adult_movement, plotting)
   
   # save the relative yield and biomasses for all areas, times after reserve
   # implementation, and control rules
@@ -41,3 +45,6 @@ for (i in 1:num_sims) {
 
 save(sims_yield, file = "../../data/sims_yield.Rda")
 save(sims_biomass, file = "../../data/sims_biomass.Rda")
+
+plot_stuff(filepath1 = "../../data/sims_yield.Rda", 
+           filepath2 = "../../data/sims_biomass.Rda", A, time2, CR)
