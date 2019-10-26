6e360e3c894bf43cd7149c5d832c3cb03d03dc75
Victoria Quennessen <viquennessen@gmail.com>
2019-10-26 10:05:33 -0700

    new function to implement sliding DR

---
+++ code/R/transient_DR.R
@@ -0,0 +1,32 @@
+transient_DR <- function(M, time1, time2, final_DR, estimate, t) {
+  
+  # calculate target_DR based on transient timescales
+  
+  # set timesteps
+  years <- 0:time2
+  
+  # set difference in M
+  difference <- 0.02
+  
+  # low M
+  if (estimate == 'low') {
+    m <- M - difference
+    y <- 1 - (1 - final_DR)*(1 - exp(-1 * m * years))
+  }
+  
+  # correct M
+  if (estimate == 'correct') {
+    y <- 1 - (1 - final_DR)*(1 - exp(-1 * M * years))
+  }
+  
+  # high M
+  if (estimate == 'high') {
+    m <- M + difference
+    y <- 1 - (1 - final_DR)*(1 - exp(-1 * m * years))
+  }
+  
+  target_DR <- y[t - time1]
+  
+  return(target_DR)
+  
+}
