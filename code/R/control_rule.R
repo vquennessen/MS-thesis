a629de40722cd676ec2fe14890d93f0b785da3ee
Victoria Quennessen <viquennessen@gmail.com>
2019-10-26 15:47:58 -0700

    kept two old control rules (one of 2 best, and worst), added 3 new ones based on moving target_DR (incorporating transients)

--- code/R/control_rule.R
+++ code/R/control_rule.R
@@ -1,35 +1,6 @@
-control_rule <- function(t, cr, E, Count, time1, transects) {
+control_rule <- function(t, cr, E, Count, time1, time2, transects, M) {
   
   if (cr == 1) {
-    E <- management(t, cr, E, DR = 0, CR_type = 'effort', target_DR = 0, 
-                    floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
-  }
-  
-  if (cr == 2) {
-    DR <- density_ratio(t, cr, Count, years_sampled = 3, 
-                        fished_areas_sampled = 'all', 
-                        fish_sampled = 'all', transects)
-    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.6, 
-                    floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
-  }
-  
-  if (cr == 3) {
-    DR <- density_ratio(t, cr, Count, years_sampled = 1, 
-                        fished_areas_sampled = 'all', 
-                        fish_sampled = 'all', transects)
-    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.6, 
-                    floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
-  }
-  
-  if (cr == 4) {
-    DR <- density_ratio(t, cr, Count, years_sampled = 1, 
-                        fished_areas_sampled = 'far', 
-                        fish_sampled = 'all', transects)
-    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.6, 
-                    floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
-  }
-  
-  if (cr == 5) {
     DR <- density_ratio(t, cr, Count, years_sampled = 1, 
                         fished_areas_sampled = 'all', 
                         fish_sampled = 'mature', transects)
@@ -37,25 +8,39 @@ control_rule <- function(t, cr, E, Count, time1, transects) {
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
-  if (cr == 6) {
+  if (cr == 2) {
+    E <- management(t, cr, E, DR = 0, CR_type = 'effort', target_DR = NA, 
+                    floor_DR = NA, effort_inc_allowed = 0.1, time1)
+  }
+  
+  if (cr == 3) {
     DR <- density_ratio(t, cr, Count, years_sampled = 1, 
                         fished_areas_sampled = 'all', 
                         fish_sampled = 'all', transects)
-    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.8, 
+    target_DR <- transient_DR(M, t, time1, time2, final_DR = 0.6, 
+                              estimate = 'low')
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
-  if (cr == 7) {
-    E <- management(t, cr, E, DR = 0, CR_type = 'effort', target_DR = 0, 
-                    floor_DR = 0.2, effort_inc_allowed = 0, time1)
-  }
-  
-  if (cr == 8) {
+  if (cr == 4) {
     DR <- density_ratio(t, cr, Count, years_sampled = 1, 
                         fished_areas_sampled = 'all', 
                         fish_sampled = 'all', transects)
-    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.8, 
-                    floor_DR = 0.2, effort_inc_allowed = 0, time1)
+    target_DR <- transient_DR(M, t, time1, time2, final_DR = 0.6, 
+                              estimate = 'correct')
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR, 
+                    floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
+  }
+  
+  if (cr == 5) {
+    DR <- density_ratio(t, cr, Count, years_sampled = 1, 
+                        fished_areas_sampled = 'all', 
+                        fish_sampled = 'all', transects)
+    target_DR <- transient_DR(M, t, time1, time2, final_DR = 0.6, 
+                              estimate = 'high')
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR, 
+                    floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
   return(E)
