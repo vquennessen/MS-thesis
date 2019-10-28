bce37d3ad4589f38bf1c22534215a95c24118a42
Victoria Quennessen <viquennessen@gmail.com>
2019-10-28 10:52:49 -0700

    updated plot_stuff.R to have input parameters and playing around with graphics, such as how much data to put in shadow

--- code/R/plot_stuff.R
+++ code/R/plot_stuff.R
@@ -1,37 +1,49 @@
-plot_stuff <- function(filepath1, filepath2, A, time2, CR) {
+plot_stuff <- function(filepath1, filepath2, A, time2, CR, sample_size) {
   
-setwd("C:/Users/Vic/Documents/Projects/DensityRatio/code/R")  
-
-library(viridis)
+  num_sims <- 1e4
+  filepath1 <- "../../data/1e4_sims_yield.Rda"
+  filepath2 <- "../../data/1e4_sims_biomass.Rda"
+  A <- 5
+  time2 <- 20
+  CR <- 5
+  sample_size <- 500
+  PD <- 0.05
   
-  load("../../data/sims_yield.Rda")
-  load("../../data/sims_biomass.Rda")
+  setwd("C:/Users/Vic/Documents/Projects/DensityRatio/code/R")  
+  
+  library(viridis)
+  
+  load(filepath1)
+  load(filepath2)
+  
+  indices <- sample(1:num_sims, sample_size)
+  Y_sample_runs <- sims_yield[, , , indices]
   
   # replace NA's in sims_yield with 0s
-  sims_yield[is.na(sims_yield)] <- 0
+  Y_sample_runs[is.na(Y_sample_runs)] <- 0
   
   # initialize median, lowerIQR, and upperIQR arrays
   Y_medians <- array(rep(NA, A*time2*CR), c(A, time2, CR))
-  Y_lowerIQR <- array(rep(NA, A*time2*CR), c(A, time2, CR))
-  Y_upperIQR <- array(rep(NA, A*time2*CR), c(A, time2, CR))
+  Y_lower <- array(rep(NA, A*time2*CR), c(A, time2, CR))
+  Y_upper <- array(rep(NA, A*time2*CR), c(A, time2, CR))
   
-  B_medians <- array(rep(NA, A*time2*CR), c(A, time2, CR))
-  B_lowerIQR <- array(rep(NA, A*time2*CR), c(A, time2, CR))
-  B_upperIQR <- array(rep(NA, A*time2*CR), c(A, time2, CR))
+  # B_medians <- array(rep(NA, A*time2*CR), c(A, time2, CR))
+  # B_lowerIQR <- array(rep(NA, A*time2*CR), c(A, time2, CR))
+  # B_upperIQR <- array(rep(NA, A*time2*CR), c(A, time2, CR))
   
   # extract data from files and plot medians + interquartile ranges
   for (i in 1:A) {
     for (j in 1:time2) {
       for (k in 1:CR) {
         
-        Y_medians[i, j, k] <- median(sims_yield[i, j, k, ])
-        B_medians[i, j, k] <- median(sims_biomass[i, j, k, ])
+        Y_medians[i, j, k] <- median(Y_sample_runs[i, j, k, ])
+#        B_medians[i, j, k] <- median(biomass[i, j, k, ])
         
-        Y_lowerIQR[i, j, k] <- quantile(sims_yield[i, j, k, ])[[2]]
-        B_lowerIQR[i, j, k] <- quantile(sims_biomass[i, j, k, ])[[2]]
-        
-        Y_upperIQR[i, j, k] <- quantile(sims_yield[i, j, k, ])[[4]]
-        B_upperIQR[i, j, k] <- quantile(sims_biomass[i, j, k, ])[[4]]
+        Y_lower[i, j, k] <- quantile(Y_sample_runs[i, j, k, ], 0.5 - PD)
+# #        B_lowerIQR[i, j, k] <- quantile(biomass[i, j, k, ])[[2]]
+#         
+        Y_upper[i, j, k] <- quantile(Y_sample_runs[i, j, k, ], 0.5 + PD)
+# #        B_upperIQR[i, j, k] <- quantile(biomass[i, j, k, ])[[4]]
         
       }
     }
@@ -46,8 +58,8 @@ library(viridis)
   par(mar = c(5.1, 4.1, 4.1, 8.7), xpd = T)
   
   # y-axis limits
-  y1 <- 0.5
-  y2 <- 1.5
+  y1 <- 0.75
+  y2 <- 1.25
   y_by <- (y2 - y1)/2
   
   # x-axis limits
@@ -92,7 +104,7 @@ library(viridis)
             lty = cr)                  # set line type
       
       polygon(x = c(1:time2, rev(1:time2)), 
-              y = c(Y_lowerIQR[a, , cr], rev(Y_upperIQR[a, , cr])), 
+              y = c(Y_lower[a, , cr], rev(Y_upper[a, , cr])), 
               col = adjustcolor(color[cr], alpha.f = 0.10), border = NA)
     }
     
@@ -107,69 +119,69 @@ library(viridis)
            cex = 0.9)                            # text size
   }
   
-  ##### Plot relative biomass + IQR over time after reserve implementation #####
-  
-  # y-axis limits
-  yy1 <- 0.5
-  yy2 <- 1.5
-  yy_by <- (yy2 - yy1)/2
-  
-  # x-axis limits
-  xx1 <- 0
-  xx2 <- time2
-  xx_by <- xx2/2
-  
-  for (a in 1:3) {
-    
-    area <- ifelse(a < 2, 'far from', ifelse(a == 3, 'in', 'near'))
-    title <- sprintf("Relative biomass range: %s reserve", area)    
-    
-    # plot the relative yield
-    plot(1, type = 'l',                          # make an empty line graph
-         main = title,                           # title of plot
-         ylab = 'Relative Biomass (metric tons)',                # axis labels
-         xlab = 'Years since marine reserve implementation',
-         xaxt = 'n', 
-         yaxt = 'n',                             # get rid of y-axis
-         xlim = c(xx1, xx2),                     # set x-axis limits
-         ylim = c(yy1, yy2)
-    )
-    
-    
-    # set specific y-axis
-    yytick <- seq(yy1, yy2, by = yy_by)          # set yaxis tick marks
-    axis(side = 2,                               # specify y axis
-         at = yytick,                            # apply tick marks
-         labels = T,                             # apply appropriate labels
-         las = 1)                                # set text horizontal
-    
-    # set specific x-axis
-    xxtick <- seq(xx1, xx2, by = xx_by)          # set x axis tick marks
-    axis(side = 1,                               # specify x axis
-         at = xxtick,                            # apply tick marks
-         labels = T,                             # apply appropriate labels
-         las = 1)                                # set text horizontal    
-    
-    for (cr in 1:CR) {                           # add one line per control rule
-      lines(B_medians[a, , cr],
-            col = color[cr],                     # use pre-defined color palette
-            lwd = 2,                     # set line width
-            lty = cr)                 # set line type
-      
-      polygon(x = c(1:time2, rev(1:time2)), 
-              y = c(B_lowerIQR[a, , cr], rev(B_upperIQR[a, , cr])), 
-              col = adjustcolor(color[cr], alpha.f = 0.10), border = NA)
-    }
-    
-    # add a legend
-    legend(x = c(21.5, 29), y = c(y2 + 0.04, y2 - (y2-y1)/2.5),   # position
-           col = color,                          # apply viridis color palette
-           lwd = 2,                              # apply line thicknesses
-           lty = 1:CR,                            # apply line patterns
-           title = 'CR',                         # add legend title and labels
-           c("CR 1", "CR 2", "CR 3", "CR 4", "CR 5"),
-           seg.len = 3.5,                        # adjust length of lines
-           cex = 0.9)                            # text size
-  }
+  # ##### Plot relative biomass + IQR over time after reserve implementation #####
+  # 
+  # # y-axis limits
+  # yy1 <- 0.5
+  # yy2 <- 1.5
+  # yy_by <- (yy2 - yy1)/2
+  # 
+  # # x-axis limits
+  # xx1 <- 0
+  # xx2 <- time2
+  # xx_by <- xx2/2
+  # 
+  # for (a in 1:3) {
+  #   
+  #   area <- ifelse(a < 2, 'far from', ifelse(a == 3, 'in', 'near'))
+  #   title <- sprintf("Relative biomass range: %s reserve", area)    
+  #   
+  #   # plot the relative yield
+  #   plot(1, type = 'l',                          # make an empty line graph
+  #        main = title,                           # title of plot
+  #        ylab = 'Relative Biomass (metric tons)',                # axis labels
+  #        xlab = 'Years since marine reserve implementation',
+  #        xaxt = 'n', 
+  #        yaxt = 'n',                             # get rid of y-axis
+  #        xlim = c(xx1, xx2),                     # set x-axis limits
+  #        ylim = c(yy1, yy2)
+  #   )
+  #   
+  #   
+  #   # set specific y-axis
+  #   yytick <- seq(yy1, yy2, by = yy_by)          # set yaxis tick marks
+  #   axis(side = 2,                               # specify y axis
+  #        at = yytick,                            # apply tick marks
+  #        labels = T,                             # apply appropriate labels
+  #        las = 1)                                # set text horizontal
+  #   
+  #   # set specific x-axis
+  #   xxtick <- seq(xx1, xx2, by = xx_by)          # set x axis tick marks
+  #   axis(side = 1,                               # specify x axis
+  #        at = xxtick,                            # apply tick marks
+  #        labels = T,                             # apply appropriate labels
+  #        las = 1)                                # set text horizontal    
+  #   
+  #   for (cr in 1:CR) {                           # add one line per control rule
+  #     lines(B_medians[a, , cr],
+  #           col = color[cr],                     # use pre-defined color palette
+  #           lwd = 2,                     # set line width
+  #           lty = cr)                 # set line type
+  #     
+  #     polygon(x = c(1:time2, rev(1:time2)), 
+  #             y = c(B_lowerIQR[a, , cr], rev(B_upperIQR[a, , cr])), 
+  #             col = adjustcolor(color[cr], alpha.f = 0.10), border = NA)
+  #   }
+  #   
+  #   # add a legend
+  #   legend(x = c(21.5, 29), y = c(y2 + 0.04, y2 - (y2-y1)/2.5),   # position
+  #          col = color,                          # apply viridis color palette
+  #          lwd = 2,                              # apply line thicknesses
+  #          lty = 1:CR,                            # apply line patterns
+  #          title = 'CR',                         # add legend title and labels
+  #          c("CR 1", "CR 2", "CR 3", "CR 4", "CR 5"),
+  #          seg.len = 3.5,                        # adjust length of lines
+  #          cex = 0.9)                            # text size
+  # }
   
 }
