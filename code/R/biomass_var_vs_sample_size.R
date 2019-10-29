95ebb1bcd1704b10910d25dd28c5119151025e2f
Victoria Quennessen <viquennessen@gmail.com>
2019-10-28 17:53:52 -0700

    deleted var_vs_sample_size.R to replace it with specific ones for yield and biomass

--- code/R/biomass_var_vs_sample_size.R
+++ code/R/biomass_var_vs_sample_size.R
@@ -2,26 +2,25 @@ library(viridis)
 
 setwd("C:/Users/Vic/Documents/Projects/DensityRatio/code/R")
 
-load("../../data/1e5_sims_yield.Rda")
-# load("../../data/1e5_sims_biomass.Rda")
+load("../../data/1e5_sims_biomass.Rda")
 
-num_sims <- 1e5
+num_sims <- 1e4
 
 # create empty plot
 par(mar=c(4.5,5,3,1))
 plot(1, type = 'l',                          # make an empty line graph
-     main = 'Variance of Yield',             # title of plot
+     main = 'Variance of Biomass',             # title of plot
      ylab = 'Sample Variance',               # axis labels
      xlab = 'Sample Size', 
      xaxt = 'n', 
      yaxt = 'n',
      xlim = c(0, num_sims),
-     ylim = c(0.1, 0.2), 
+     ylim = c(0, 0.05), 
      cex.main = 2, 
      cex.lab = 2)
 
 # set specific y-axis
-ytick <- seq(0.1, 0.2, by = 0.05)            # set y axis tick marks
+ytick <- seq(0, 0.05, by = 0.025)            # set y axis tick marks
 axis(side = 2,                               # specify y axis
      at = ytick,                             # apply tick marks
      labels = T,                             # apply appropriate labels
@@ -41,26 +40,23 @@ time2 <- 20
 color <- viridis(num_runs)
 
 # set different sample sizes
-sample_size <- seq(1e2, 1e5, by = 1e2)
+sample_size <- seq(1e3, num_sims, by = 1e2)
 
 # initialize variance arrays 
-Y_vars <- rep(NA, length(sample_size))
-# B_vars <- rep(NA, length(sample_size))
+B_vars <- rep(NA, length(sample_size))
 
 for (j in 1:num_runs) {
   
   for (i in 1:length(sample_size)) {
     
     indices <- sample(1:num_sims, sample_size[i])
-    sampled_yield <- sims_yield[, , , indices]
-    # sampled_biomass <- sims_biomass[, , , indices]
+    sampled_biomass <- sims_biomass[, , , indices]
     
     # calculate medians 
-    Y_vars[i] <- var(sampled_yield[1, time2, 1, ])
-    # B_vars[i] <- var(sampled_biomass[1, time2, 1, ])
+    B_vars[i] <- var(sampled_biomass[1, time2, 1, ])
     
   }
   
   # plot one run through various sample sizes
-  lines(sample_size, Y_vars, type = 'l', col = color[j])
+  lines(sample_size, B_vars, type = 'l', col = color[j])
 }
