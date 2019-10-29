af7404d853b77a954e55475bf7562e0defd2680d
Victoria Quennessen <viquennessen@gmail.com>
2019-10-29 16:56:20 -0700

    pop_dynamics.R - only update fishing mortality if fishing == T

--- code/R/pop_dynamics.R
+++ code/R/pop_dynamics.R
@@ -1,40 +1,11 @@
-#' Population Dynamics, step forward one year in time
-#'
-#' @param a area, a number (1, A)
-#' @param t time step, a number (1, time)
-#' @param SSB spawning stock biomass, a 2D numeric array (dimensions area x time)
-#' @param N age-structured population, a 3D numeric array 
-#' (dimensions age x area x time)
-#' @param W weight at age, a numeric vector (length age)
-#' @param Mat fraction mature at age, a numeric vector (length age)
-#' @param A total number of areas, a number ( > 0)
-#' @param R0 unfished recruitment, a number ( > 0)
-#' @param h steepness parameter, a number
-#' @param B0 unfished spawning stock biomass, a number ( > 0)
-#' @param e epsilon, a numeric vector (length time)
-#' @param sigma_R recruitment standard deviation, a number ( > 0)
-#' @param Fb fully selected fishing mortality rate on the whole population 
-#' before reserve implementation, a number (0, 1)
-#' @param E nominal fishing effort in each area before reserve implementation,
-#' a number (0, 1)
-#' @param S selectivity at age, a numeric vector (length age)
-#' @param M natural mortality, a number (0, 1)
-#'
-#' @return B, R, FM, N - updated spawning stock biomass, recruitment, fishing
-#' mortality, and age-structured population size, after 1 time step
-#' @export
-#'
-#' @examples
-
 pop_dynamics <- function(a, t, cr, rec_age, max_age, n, SSB, N, W, Mat, A, R0, 
                          h, B0, Eps, sigma_R, Fb, E, S, M, FM, m, abundance_all, 
-                         abundance_mature, biomass) {
-  
-  # Calculate spawning stock biomass
-  SSB[a, t, cr] <- spawning_stock_biomass(a, t, cr, rec_age, N, W, Mat)
+                         abundance_mature, biomass, fishing) {
   
   # Calculate fishing mortality
-  FM[, a, t, cr] <- fishing_mortality(a, t, cr, FM, A, Fb, E, S)
+  if (fishing == T) {
+    FM[, a, t, cr] <- fishing_mortality(a, t, cr, FM, A, Fb, E, S)
+  } else { FM[, a, t, cr] <- 0 }
   
   ##### Step population foward in time
   
@@ -50,11 +21,17 @@ pop_dynamics <- function(a, t, cr, rec_age, max_age, n, SSB, N, W, Mat, A, R0,
   N[n, a, t, cr] <- N[n - 1, a, t - 1, cr] * exp(-1 * (FM[n - 1, a, t - 1, cr] + M)) + 
     N[n, a, t - 1, cr] * exp(-1 * (FM[n, a, t - 1, cr] + M)) 
   
+  # Calculate abundance of all fish
   abundance_all[a, t, cr] <- sum(N[, a, t, cr])
   
+  # Calculate abundance of mature fish
   abundance_mature[a, t, cr] <- sum(N[m:n, a, t, cr])
   
+  # Calculate biomass of all fish
   biomass[a, t, cr] <- sum(N[, a, t, cr] * W)
+  
+  # Calculate spawning stock biomass
+  SSB[a, t, cr] <- sum(N[, a, t, cr]*W*Mat)
   
   output <- list(SSB, FM, N, abundance_all, abundance_mature, biomass)
   
