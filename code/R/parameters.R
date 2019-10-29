bcf9991f9152e499fda62c1ccfcdf5088e34a0ce
Victoria Quennessen <viquennessen@gmail.com>
2019-10-28 17:55:36 -0700

    got rid of useless male growth parameters

--- code/R/parameters.R
+++ code/R/parameters.R
@@ -19,11 +19,8 @@ parameters = function(species) {
     M <- 0.14                               # natural mortality
     rec_age <- 2                            # age at recruitment
     af <- 1.68e-5; bf <- 3                  # weight at length parameters (f)
-    am <- 1.68e-5; bm <- 3                  # weight at length parameters (m)
     a1f <- 5; L1f <- 32.21;                 # growth parameters (f)
     a2f <- 15; L2f <- 47.95; Kf <- 0.2022  
-    a1m <- 5; L1m <- 31.88;                 # growth parameters (m)
-    a2m <- 15; L2m <- 45.39; Km <- 0.1979  
     L50 <- 39.53                            # length at 50% maturity
     k_mat <- -0.4103                        # slope of maturity curve
     ldp <- 0.1                              # larval drift proportion
@@ -67,11 +64,8 @@ parameters = function(species) {
     M <- 0.17                               # natural mortality
     rec_age <- 3                            # age at recruitment
     af <- 2.6e-5; bf <- 2.88                # weight at length parameters (f)
-    am <- 2.58e-5; bm <- 2.89               # weight at length parameters (m)
     a1f <- 1; L1f <- 20.32;                 # growth parameters (f)
     a2f <- 40; L2f <- 49.67; Kf <- 0.21  
-    a1m <- 1; L1m <- 17.47;                 # growth parameters (m)
-    a2m <- 40; L2m <- 43.27; Km <- 0.34  
     L50 <- 43.69                            # length at 50% maturity
     k_mat <- -0.66                          # slope of maturity curve
     ldp <- 0.1                              # larval drift proportion
@@ -104,18 +98,16 @@ parameters = function(species) {
     L50_down <- c(15, 13, 13, 12, 6.5)      # L50 value for downcurve
     cf <- c(0.0001, 0.1679, 0.0982,         # fraction of fishery caught / fleet
             0.6979, 0.0358)               
-    switch <- c(0, 0, 0, 0, 0)
-    full <- c(0, 0, 0, 0, 0)
     catch_form <- 'continuous'              # discrete or continuous catch
     season <- 0.5                           # if catch_formulation = discrete, 
                                             #       time at which fishing occurs
                                             #       0 = beginning, 1 = end
   }
   
-  output = list(max_age, M, rec_age, af, bf, am, bm, a1f, L1f, a2f, L2f, Kf,
-                a1m, L1m, a2m, L2m, Km, L50, k_mat, ldp, h, phi, sigma_R,
-                rho_R, AMP, D, Fb, r, x, sp, c, b, fleets, alpha, beta, start, 
-                F_fin, L50_up, L50_down, cf, switch, full, catch_form, season)
+  output = list(max_age, M, rec_age, af, bf, a1f, L1f, a2f, L2f, Kf, L50, k_mat, 
+                ldp, h, phi, sigma_R, rho_R, AMP, D, Fb, r, x, sp, c, b, fleets, 
+                alpha, beta, start, F_fin, L50_up, L50_down, cf, catch_form, 
+                season)
   
   return(output)
   
