d6008ad09869115e406a0a81c6d5e1bf9089e9e0
Victoria Quennessen <viquennessen@gmail.com>
2019-10-29 17:01:14 -0700

    deleted effort allocation before reserve implementation in effort_allocation.R

--- code/R/effort_allocation.R
+++ code/R/effort_allocation.R
@@ -8,22 +8,11 @@ effort_allocation <- function(t, cr, allocation, A, E, yield, time1) {
   #' If effort is allocated using the ideal free distribution, effort for one 
   #' year depends on the distribution of yield from the previous year
   if (allocation == 'IFD') {
-    
-    # Before reserve implementation
-    if (t < time1) {
-      
-      prop_yield <- yield[, t - 1, cr]/sum(yield[, t - 1, cr])
-      E[, t, cr] <- sum(E[, t - 1, cr])*prop_yield
-      
-      #' After reserve implementation, effort in the reserve is equal to zero
-    } else if (t >= time1) {
       
       prop_yield_outside <- yield[outside, t - 1, cr]/sum(yield[outside, t - 1, cr])
       
       E[outside, t, cr] <- sum(E[, t - 1, cr])*prop_yield_outside
       E[reserve, t, cr] <- 0
-      
-    }
     
   #' Otherwise, distribute effort equally between the four areas outside the 
   #' marine reserve, regardless of yield
