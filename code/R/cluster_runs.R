3cc133d17ca084b2c60e7393b9bb28bb1faddbdf
Victoria Quennessen <viquennessen@gmail.com>
2019-10-29 18:31:19 -0700

    edited cluster_runs.R to run based on new updates

--- code/R/cluster_runs.R
+++ code/R/cluster_runs.R
@@ -1,5 +1,3 @@
-setwd("C:/Users/Vic/Documents/Projects/DensityRatio/code/R")
-
 source('./base_model.R')
 source('./plot_stuff.R')
 
@@ -10,7 +8,7 @@ time2 <- 20
 CR <- 5
 allocation <- 'IFD'
 R0 <- 1e+5
-stochasticity <- F
+stochasticity <- T
 surveys <- T
 transects <- 24
 fishery_management <- T
@@ -19,7 +17,7 @@ adult_movement <- T
 plotting <- F
 
 # set numbers of simulations
-num_sims <- 1e+02
+num_sims <- 1e4
 
 # initialize yield and biomass arrays
 sims_yield <- array(rep(0, A*time2*CR*num_sims), c(A, time2, CR, num_sims))
@@ -30,8 +28,8 @@ sims_SSB <- array(rep(0, A*time2*CR*num_sims), c(A, time2, CR, num_sims))
 for (i in 1:num_sims) {
   
   output <- base_model(species, A, time1, time2, CR, allocation, R0, 
-             stochasticity, surveys, transects, fishery_management, 
-             fishing, adult_movement, plotting)
+                       stochasticity, surveys, transects, fishery_management, 
+                       fishing, adult_movement, plotting)
   
   # save the relative yield and biomasses for all areas, times after reserve
   # implementation, and control rules
@@ -41,6 +39,13 @@ for (i in 1:num_sims) {
   
 }
 
-save(sims_yield, file = "../../data/1e3_sims_yield_noStochasticity.Rda")
-save(sims_biomass, file = "../../data/1e3_sims_biomass_noStochasticity.Rda")
-save(sims_SSB, file = "../../data/1e3_sims_SSB_noStochasticity.Rda")
+filepath1 = "../../data/1e4_sims_yield.Rda"
+filepath2 = "../../data/1e4_sims_biomass.Rda"
+filepath3 = "../../data/1e4_sims_SSB.Rda"
+
+save(sims_yield, file = filepath1)
+save(sims_biomass, file = filepath2)
+save(sims_SSB, file = filepath3)
+
+plot_stuff(filepath1, filepath2, filepath3, A, time2, CR, num_sims, 
+           sample_size = num_sims, PD = 0.25)
