f8ad3f4d950e1411174ff77d503c3d3b68f09652
Victoria Quennessen <viquennessen@gmail.com>
2019-10-30 08:51:15 -0700

    added nm dimentionality to sampling.R

--- code/R/sampling.R
+++ code/R/sampling.R
@@ -1,14 +1,14 @@
-sampling <- function(a, t, cr, Delta, Gamma, abundance_all, abundance_mature, 
-                     transects, x, Count, nuS) {
+sampling <- function(a, t, cr, nm, Delta, Gamma, abundance_all, 
+                     abundance_mature, transects, x, Count, nuS) {
   
   # Total population size across all areas
-  total_all <- sum(abundance_all[, t, cr])
-  total_mature <- sum(abundance_mature[, t, cr])
+  total_all <- sum(abundance_all[, t, cr, nm])
+  total_mature <- sum(abundance_mature[, t, c, nm])
   
   # Calculate odds ratio of seeing a fish
   # Based on Babcock & MacCall (2011): Eq. (12)
-  odds_all <-  (Delta * abundance_all[, t, cr]) / (total_all / A)
-  odds_mature <-  (Delta * abundance_mature[, t, cr]) / (total_mature / A)
+  odds_all <-  (Delta * abundance_all[, t, cr, nm]) / (total_all / A)
+  odds_mature <-  (Delta * abundance_mature[, t, cr, nm]) / (total_mature / A)
   
   # Calculate probability based on odds ratio
   p_all <- 1 / (1 + exp(odds_all))
@@ -20,8 +20,8 @@ sampling <- function(a, t, cr, Delta, Gamma, abundance_all, abundance_mature,
   presence_mature <- rbinom(transects, 1, p_mature)
   
   # Calculate species count given transects with positive visuals
-  Count[a, t, , 1, cr] <- presence_all*(Gamma*abundance_all[a, t, cr]*exp(nuS[a, t, cr]))
-  Count[a, t, , 2, cr] <- presence_mature*(Gamma*abundance_mature[a, t, cr]*exp(nuS[a, t, cr]))
+  Count[a, t, , 1, cr, nm] <- presence_all*(Gamma*abundance_all[a, t, cr, nm]*exp(nuS[a, t, cr, nm]))
+  Count[a, t, , 2, cr, nm] <- presence_mature*(Gamma*abundance_mature[a, t, cr, nm]*exp(nuS[a, t, cr, nm]))
   
-  return(Count[a, t, , , cr])
+  return(Count[a, t, , , cr, nm])
 }
