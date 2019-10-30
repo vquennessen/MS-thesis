77da60026d019e0ae81e35eb4554d24096230350
Victoria Quennessen <viquennessen@gmail.com>
2019-10-30 12:24:48 -0700

    updated to include nm dimensions in transient_DR.R

--- code/R/transient_DR.R
+++ code/R/transient_DR.R
@@ -1,30 +1,19 @@
-transient_DR <- function(M, t, time1, time2, final_DR, estimate) {
+transient_DR <- function(nat_mortality, t, time1, time2, final_DR, estimate) {
   
   # calculate target_DR based on transient timescales
+  
+  nm <- ifelse(estimate == 'low', 1, ifelse(estimate == 'correct', 2, 3))
   
   # set timesteps
   years <- 0:time2
   
   # set difference in M
   difference <- 0.05
-  
-  # low M
-  if (estimate == 'low') {
-    m <- M - difference
-    y <- 1 - (1 - final_DR)*(1 - exp(-1 * m * years))
-  }
-  
-  # correct M
-  if (estimate == 'correct') {
-    y <- 1 - (1 - final_DR)*(1 - exp(-1 * M * years))
-  }
-  
-  # high M
-  if (estimate == 'high') {
-    m <- M + difference
-    y <- 1 - (1 - final_DR)*(1 - exp(-1 * m * years))
-  }
-  
+
+  # calculate moving DR
+  y <- 1 - (1 - final_DR)*(1 - exp(-1 * nat_mortality[nm] * years))
+    
+  # target DR depends on what time step we are on
   target_DR <- y[t - time1]
   
   return(target_DR)
