f4567c6ac149de2dfae9a3ff9d15f702bbc62cbb
Victoria Quennessen <viquennessen@gmail.com>
2019-10-30 08:51:43 -0700

    added nm dimentionality to epsilon.R

--- code/R/epsilon.R
+++ code/R/epsilon.R
@@ -9,23 +9,25 @@
 #'
 #' @examples
 
-epsilon <- function (A, timeT, CR, nuR, rho_R) {
+epsilon <- function (A, timeT, CR, NM, nuR, rho_R) {
   
   # Error term for recruitment
   # Based on Babcock & MacCall (2011): Eq. (4)
   
   # initialize epsilon vector
-  # Dimensions = area * timeT * CR
-  Eps <- array(rep(0, A*timeT*CR), c(A, timeT, CR))
+  # Dimensions = area * timeT * CR * M values (3)
+  Eps <- array(rep(0, A*timeT*CR*NM), c(A, timeT, CR, NM))
   
   # eps[, 1, ]
-  Eps[, 1, ] <- nuR[, 1, ]*sqrt(1 + rho_R^2)
+  Eps[, 1, , ] <- nuR[, 1, , ]*sqrt(1 + rho_R^2)
   
   # fill in rest of epsilon vector
   for (a in 1:A) {
     for (t in 2:timeT) {
       for (cr in 1:CR) {
-        Eps[a, t, cr] <- rho_R*Eps[a, t-1, cr] + nuR[a, t, cr]*sqrt(1 + rho_R^2)
+        for (nm in 1:NM) {
+        Eps[a, t, cr, nm] <- rho_R*Eps[a, t-1, cr, nm] + nuR[a, t, cr, nm]*sqrt(1 + rho_R^2)
+        }
       }
     }
   }
