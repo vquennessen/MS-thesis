89bf54f76508dc8d6cb04e76caffa0ec7b0487c3
Victoria Quennessen <viquennessen@gmail.com>
2019-10-30 12:28:30 -0700

    continued updating equilibrium_SAD.R to include nm dimensions in

--- code/R/equilibrium_SAD.R
+++ code/R/equilibrium_SAD.R
@@ -1,77 +1,70 @@
-equilibrium_SAD <- function(a, cr, nm, A, rec_age, max_age, n, W, R0,
+equilibrium_SAD <- function(a, cr, A, rec_age, max_age, n, W, R0,
                             Mat, h, B0, Eps, sigma_R, Fb, S, M, eq_time, m, 
-                            stochasticity, rho_R, NM, nat_mortality) {
-  
-  # Initialize SADs
-  SADs <- array(rep(0, n*NM), c(n, NM))
+                            stochasticity, rho_R, nat_mortality) {
   
   # Initialize population size array
-  # Dimensions = age * 1 * time * 1 * M values (3)
-  N2 <- FM2 <- array(rep(0, n*eq_timea*NM), c(n, 1, eq_time, 1, NM))
+  # Dimensions = age * 1 * time * 1
+  N2 <- FM2 <- array(rep(0, n*eq_timea), c(n, 1, eq_time, 1))
   
   # Initialize biomass and SSB arrays
-  # Dimensions = 1 * time * 1 * M values (3)
-  biomass2 <- SSB2 <- array(rep(0, eq_time*NM), c(1, eq_time, 1, NM))
-  abundance_all2 <- abundance_mature2 <- array(rep(0, eq_time*NM), 
-                                               c(1, eq_time, 1, NM))
+  # Dimensions = 1 * time * 1
+  biomass2 <- SSB2 <- array(rep(0, eq_time), c(1, eq_time, 1))
+  abundance_all2 <- abundance_mature2 <- array(rep(0, eq_time), 
+                                               c(1, eq_time, 1))
   
   # Recruitment normal variable
-  # Dimensions = area * timeT * CR * M values (3)
+  # Dimensions = area * timeT * CR 
   if (stochasticity == T) {
-    nuR2 <- array(rnorm(eq_time*NM, 0, sigma_R), c(1, eq_time, 1, NM))
+    nuR2 <- array(rnorm(eq_time, 0, sigma_R), c(1, eq_time, 1))
   } else if (stochasticity == F) {
-    nuR2 <- array(rep(0, eq_time*NM), c(1, eq_time, 1, NM))
+    nuR2 <- array(rep(0, eq_time), c(1, eq_time, 1))
   }
   
   # Recruitment error
-  # Dimensions = area * timeT * CR * M values (3)
-  Eps2 <- epsilon(A = 1, eq_time, CR = 1, NM, nuR2, rho_R)
+  # Dimensions = area * timeT * CR
+  Eps2 <- epsilon(A = 1, eq_time, CR = 1, NM = 1, nuR2, rho_R)
   
-  for (nm in 1:NM) {
+  # Start each age class with 10 individuals
+  # Enter FM, N, abundance, and biomasses for time = 1 to rec_age
+  # Dimensions = age * area * time * CR * M values (3)
+  for (t in 1:(rec_age + 1)) {
+    N2[, a, t, cr] <- rep(100, n)
+    biomass2[a, t, cr] <- sum(N2[, a, t, cr] * W)
+    SSB2[a, t, cr] <- sum(N2[, a, t, cr]*W*Mat)
+    abundance_all2[a, t, cr] <- sum(N2[, a, t, cr])
+    abundance_mature2[a, t, cr] <- sum(N2[m:n, a, t, cr])
+  }
+  
+  # Step population forward in time with set fishing level
+  for (t in (rec_age + 1):(eq_time - 1)) {
     
-    # Start each age class with 10 individuals
-    # Enter FM, N, abundance, and biomasses for time = 1 to rec_age
-    # Dimensions = age * area * time * CR * M values (3)
-    for (t in 1:(rec_age + 1)) {
-      N2[, a, t, cr, nm] <- rep(100, n)
-      biomass2[a, t, cr, nm] <- sum(N2[, a, t, cr, nm] * W)
-      SSB2[a, t, cr, nm] <- sum(N2[, a, t, cr, nm]*W*Mat)
-      abundance_all2[a, t, cr, nm] <- sum(N2[, a, t, cr, nm])
-      abundance_mature2[a, t, cr, nm] <- sum(N2[m:n, a, t, cr, nm])
-    }
+    # biology
+    PD <- pop_dynamics(a = 1, t, cr = 1, nm = 2, rec_age, max_age, n, SSB2, 
+                       N2, W, Mat, A, R0, h, B0, Eps2, sigma_R, Fb, E2, S, M, 
+                       NM, FM2, m, abundance_all2, abundance_mature2, 
+                       biomass2, fishing = F, nat_mortality)
     
-    # Step population forward in time with set fishing level
-    for (t in (rec_age + 1):(eq_time - 1)) {
-      
-      # biology
-      PD <- pop_dynamics(a = 1, t, cr = 1, nm, rec_age, max_age, n, SSB2, N2, W, 
-                         Mat, A, R0, h, B0, Eps2, sigma_R, Fb, E2, S, M, NM, FM2, 
-                         m, abundance_all2, abundance_mature2, biomass2, 
-                         fishing = F, nat_mortality)
-      
-      SSB2                <- PD[[1]]
-      N2                  <- PD[[3]]
-      abundance_all2      <- PD[[4]]
-      abundance_mature2   <- PD[[5]]
-      biomass2            <- PD[[6]]
-      
-    }
-    
-    # plotting for troubleshooting
-    # plot(1:eq_time, N2[1, 1, 1:eq_time, 1], type = 'l', ylim = c(0, 2e4), col = 'green')
-    # for (x in 2:(n - 1)) {
-    #   lines(1:eq_time, N2[x, 1, 1:eq_time, 1], col = 'red')
-    # }
-    # lines(1:eq_time, N2[n, 1, 1:eq_time, 1], col = 'blue')
-    
-    SADs[, nm] <- N2[, 1, eq_time - 1, 1, nm]
+    SSB2                <- PD[[1]]
+    N2                  <- PD[[3]]
+    abundance_all2      <- PD[[4]]
+    abundance_mature2   <- PD[[5]]
+    biomass2            <- PD[[6]]
     
   }
   
-  # # Adjust stable age distribution to give total biomass of B0
-  # B1 <- as.numeric(SAD %*% W)
-  # SAD1 <- SAD * (B0 / B1)
+  # plotting for troubleshooting
+  # plot(1:eq_time, N2[1, 1, 1:eq_time, 1], type = 'l', ylim = c(0, 2e4), col = 'green')
+  # for (x in 2:(n - 1)) {
+  #   lines(1:eq_time, N2[x, 1, 1:eq_time, 1], col = 'red')
+  # }
+  # lines(1:eq_time, N2[n, 1, 1:eq_time, 1], col = 'blue')
   
-  return(SADs)
+  SAD <- N2[, 1, eq_time - 1, 1]
   
 }
+
+# # Adjust stable age distribution to give total biomass of B0
+# B1 <- as.numeric(SAD %*% W)
+# SAD1 <- SAD * (B0 / B1)
+
+return(SAD)
