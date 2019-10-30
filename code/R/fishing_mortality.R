d34be0be1c746a018cf0801482fe5bde46f39e74
Victoria Quennessen <viquennessen@gmail.com>
2019-10-30 08:52:05 -0700

    added nm dimentionality to fishing_mortality.R

--- code/R/fishing_mortality.R
+++ code/R/fishing_mortality.R
@@ -1,8 +1,8 @@
-fishing_mortality <- function(a, t, cr, FM, A, Fb, E, S) {
+fishing_mortality <- function(a, t, cr, nm, FM, A, Fb, E, S) {
   
   # Catchability (Vulnerability to fishing gear)
   # Based on Babcock & MacCall (2011): Eq. (6)
-  vulnerability <- (A*Fb)/(sum(E[, 1, 1]))
+  vulnerability <- (A*Fb)/(sum(E[, 1, 1, 1]))
   
   # Selectivity as a matrix
   # dimensions = age * 1
@@ -10,13 +10,13 @@ fishing_mortality <- function(a, t, cr, FM, A, Fb, E, S) {
   
   # Effort as a matrix
   # Dimensions = area * time * CR
-  effort <- E[a, t, cr]
+  effort <- E[a, t, cr, nm]
   
   # Fishing mortality
   # Based on Babcock & MacCall (2011): Eq. (5)
   # Dimensions = age * area * time * CR
-  FM[, a, t, cr] <- vulnerability * selectivity * effort
+  FM[, a, t, cr, nm] <- vulnerability * selectivity * effort
   
-  return(FM[, a, t, cr])
+  return(FM[, a, t, cr, nm])
   
 }
