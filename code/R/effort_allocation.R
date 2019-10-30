a1bf5a1ddcadcebf15b4c842145304c9abc80633
Victoria Quennessen <viquennessen@gmail.com>
2019-10-30 08:51:35 -0700

    added nm dimentionality to effort_allocation.R

--- code/R/effort_allocation.R
+++ code/R/effort_allocation.R
@@ -1,4 +1,4 @@
-effort_allocation <- function(t, cr, allocation, A, E, yield, time1) {
+effort_allocation <- function(t, cr, nm, allocation, A, E, yield, time1) {
   
   # separate areas inside and outside of reserve
   areas <- 1:A
@@ -9,10 +9,10 @@ effort_allocation <- function(t, cr, allocation, A, E, yield, time1) {
   #' year depends on the distribution of yield from the previous year
   if (allocation == 'IFD') {
       
-      prop_yield_outside <- yield[outside, t - 1, cr]/sum(yield[outside, t - 1, cr])
+      prop_yield_outside <- yield[outside, t - 1, cr, nm]/sum(yield[outside, t - 1, cr, nm])
       
-      E[outside, t, cr] <- sum(E[, t - 1, cr])*prop_yield_outside
-      E[reserve, t, cr] <- 0
+      E[outside, t, cr, nm] <- sum(E[, t - 1, cr, nm])*prop_yield_outside
+      E[reserve, t, cr, nm] <- 0
     
   #' Otherwise, distribute effort equally between the four areas outside the 
   #' marine reserve, regardless of yield
@@ -20,12 +20,12 @@ effort_allocation <- function(t, cr, allocation, A, E, yield, time1) {
     
     if (t < time1) {
       
-      E[, t, cr] <- rep(sum(E[, t, cr])/A, A)
+      E[, t, cr] <- rep(sum(E[, t, cr, nm])/A, A)
       
     } else if (t >= time1) {
     
-    E[reserve, t, cr] <- 0
-    E[outside, t, cr] <- rep(sum(E[, t-1, cr])/(A-1), A-1)
+    E[reserve, t, cr, nm] <- 0
+    E[outside, t, cr, nm] <- rep(sum(E[, t - 1, cr, nm])/(A - 1), A - 1)
     
     }
     
