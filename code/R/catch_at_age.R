f2bbca87794983b5183bf6867a58910c1b843954
Victoria Quennessen <viquennessen@gmail.com>
2019-10-30 08:51:26 -0700

    added nm dimentionality to catch_at_age.R

--- code/R/catch_at_age.R
+++ code/R/catch_at_age.R
@@ -1,14 +1,15 @@
-catch_at_age <- function(a, t, cr, FM, M, N, A, Fb, E, catch) {
+catch_at_age <- function(a, t, cr, nm, FM, nat_mortality, N, A, Fb, E, catch) {
   
     #' Based on the continuous (Baranov) formulation of of selectivity, i.e. that
     #' fishing mortality (FM) and natural mortality (M) are proportional to fish 
     #' abundance (N) and act simultaneously  and uniformly throughout the year, 
     #' i.e. dN/dt = -(M+F)*N
     
-    coeff <- FM[ , a, t, cr]/(M + FM[ , a, t, cr])
+    coeff <- FM[ , a, t, cr, nm]/(nat_mortality[nm] + FM[ , a, t, cr, nm])
     
-    catch[ , a, t, cr] <- coeff * N[ , a, t, cr] * exp(-1*M - FM[ , a, t, cr])
+    catch[ , a, t, cr, nm] <- coeff * N[ , a, t, cr, nm] * exp(-1*nat_mortality[nm] 
+                                                               - FM[ , a, t, cr])
   
-  return(catch[, a, t, cr])
+  return(catch[, a, t, cr, nm])
   
 }
