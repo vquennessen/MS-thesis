5951cd1368d6ee0cc89c5f92cd82a0ae10b35fd2
Victoria Quennessen <viquennessen@gmail.com>
2019-10-30 08:52:53 -0700

    added nm dimentionality to recruitment.R

--- code/R/recruitment.R
+++ code/R/recruitment.R
@@ -1,4 +1,4 @@
-recruitment = function(a, t, cr, SSB, A, R0, h, B0, Eps, sigma_R, rec_age) {
+recruitment = function(a, t, cr, nm, SSB, A, R0, h, B0, Eps, sigma_R, rec_age) {
   
   # Recruitment
   # Based on Babcock & MacCall (2011): Eq. (3)
@@ -7,9 +7,9 @@ recruitment = function(a, t, cr, SSB, A, R0, h, B0, Eps, sigma_R, rec_age) {
   adjR0 <- R0 / A
   adjB0 <- B0 / A
   
-  R1 <- (0.8 * adjR0 * h * SSB[a, t - rec_age, cr]) / (0.2 * adjB0 * (1 - h) + 
-                                              (h - 0.2) * SSB[a, t - rec_age, cr]) 
-  R <- R1 * (exp(Eps[a, t, cr] - sigma_R^2 / 2))
+  R1 <- (0.8 * adjR0 * h * SSB[a, t - rec_age, cr, nm]) / (0.2 * adjB0 * (1 - h) + 
+                                              (h - 0.2) * SSB[a, t - rec_age, cr, nm]) 
+  R <- R1 * (exp(Eps[a, t, cr, nm] - sigma_R^2 / 2))
 
   return(R)
   
