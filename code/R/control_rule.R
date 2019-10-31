dfb98d490d92d6706422fa60f191c264bf3fcd65
Victoria Quennessen <victoria.quennessen@oregonstate.edu>
2019-10-31 07:37:50 -0700

    edited to have 6 control rules: babcock and maccall best one with low, correct, and high natural mortality, and transient one with low, correct, and high natural mortality

--- code/R/control_rule.R
+++ code/R/control_rule.R
@@ -1,62 +1,54 @@
-control_rule <- function(t, cr, nm, E, Count, time1, time2, transects, M) {
+control_rule <- function(t, cr, nm, E, Count, time1, time2, transects, 
+                         nat_mortality) {
   
   if (cr == 1) {
-    estimate <- 'low'
-    DR <- density_ratio(t, cr, estimate, Count, years_sampled = 1, 
+    DR <- density_ratio(t, cr, nm = 1, Count, years_sampled = 1, 
                         fished_areas_sampled = 'all', 
                         fish_sampled = 'mature', transects)
-    E <- management(t, cr, estimate, E, DR, CR_type = 'DR', target_DR = 0.6, 
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.6, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
   if (cr == 2) {
-    estimate <- 'correct'
-    DR <- density_ratio(t, cr, estimate, Count, years_sampled = 1, 
+    DR <- density_ratio(t, cr, nm = 2, Count, years_sampled = 1, 
                         fished_areas_sampled = 'all', 
                         fish_sampled = 'mature', transects)
-    E <- management(t, cr, estimate, E, DR, CR_type = 'DR', target_DR = 0.6, 
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.6, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
   if (cr == 3) {
-    estimate <- 'high'
-    DR <- density_ratio(t, cr, estimate, Count, years_sampled = 1, 
+    DR <- density_ratio(t, cr, nm = 3, Count, years_sampled = 1, 
                         fished_areas_sampled = 'all', 
                         fish_sampled = 'mature', transects)
-    E <- management(t, cr, estimate, E, DR, CR_type = 'DR', target_DR = 0.6, 
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.6, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
   if (cr == 4) {
-    estimate <- 'low'
-    DR <- density_ratio(t, cr, estimate, Count, years_sampled = 1, 
+    DR <- density_ratio(t, cr, nm = 1, Count, years_sampled = 1, 
                         fished_areas_sampled = 'all', 
                         fish_sampled = 'mature', transects)
-    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.6, 
-                              estimate)
-    E <- management(t, cr, nm, E, DR, CR_type = 'DR', target_DR, 
+    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.6, nm)
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
   if (cr == 5) {
-    estimate <- 'correct'
-    DR <- density_ratio(t, cr, estimate, Count, years_sampled = 1, 
+    DR <- density_ratio(t, cr, nm = 2, Count, years_sampled = 1, 
                         fished_areas_sampled = 'all', 
                         fish_sampled = 'mature', transects)
-    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.6, 
-                              estimate)
-    E <- management(t, cr, nm, E, DR, CR_type = 'DR', target_DR, 
+    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.6, nm)
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
   if (cr == 6) {
-    estimate <- 'correct'
-    DR <- density_ratio(t, cr, estimate, Count, years_sampled = 1, 
+    DR <- density_ratio(t, cr, nm = 3, Count, years_sampled = 1, 
                         fished_areas_sampled = 'all', 
                         fish_sampled = 'mature', transects)
-    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.6, 
-                              estimate)
-    E <- management(t, cr, nm, E, DR, CR_type = 'DR', target_DR, 
+    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.6, nm)
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
