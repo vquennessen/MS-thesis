89ab4b7c1b9d96f0f27403f131daf0b3df5cedc8
Victoria Quennessen <victoria.quennessen@oregonstate.edu>
2019-10-31 07:39:47 -0700

    recruitment.R all on one line

--- code/R/recruitment.R
+++ code/R/recruitment.R
@@ -7,8 +7,7 @@ recruitment = function(a, t, cr, nm, SSB, A, R0, h, B0, Eps, sigma_R, rec_age) {
   adjR0 <- R0 / A
   adjB0 <- B0 / A
   
-  R1 <- (0.8 * adjR0 * h * SSB[a, t - rec_age, cr, nm]) / (0.2 * adjB0 * (1 - h) + 
-                                              (h - 0.2) * SSB[a, t - rec_age, cr, nm]) 
+  R1 <- (0.8 * adjR0 * h * SSB[a, t - rec_age, cr, nm]) / (0.2 * adjB0 * (1 - h) + (h - 0.2) * SSB[a, t - rec_age, cr, nm]) 
   R <- R1 * (exp(Eps[a, t, cr, nm] - sigma_R^2 / 2))
 
   return(R)
