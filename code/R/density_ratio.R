1bfdc7db3527e18907f1c72b5dfbb41aa6f33b5b
Victoria Quennessen <victoria.quennessen@oregonstate.edu>
2019-10-31 08:38:22 -0700

    updating DR calculation to use data from the year before, to avoid situation where the appropriate cr and nm combination haven't been evaluated yet (density_ratio.R)

--- code/R/density_ratio.R
+++ code/R/density_ratio.R
@@ -1,33 +1,33 @@
-density_ratio <- function (t, cr, nm, Count, years_sampled, 
+density_ratio <- function (t, cr, nm, A, Count, years_sampled, 
                            fished_areas_sampled, fish_sampled, transects) {
   
   # set marine reserve area as middle area
   areas <- 1:A
   inside <- median(1:A)
-  y <- areas[-inside]  
+  outside <- areas[-inside]  
   
   # sample all fish or just mature fish
   if (fish_sampled == "all") {
     fish <- 1
-  } else {
+  } else if (fish_sampled == 'mature') {
     fish <- 2
   }
   
   # calculate count inside marine reserve
-  count_in <- Count[inside, t, , fish, cr, nm]
+  count_in <- Count[inside, t - 1, , fish, cr, nm]
   
   # time sampled = 1 or 3 years
   if (years_sampled == 1) {
-    years <- t
+    years <- t - 1
   } else {
-    years <- (t - years_sampled + 1):t
+    years <- (t - years_sampled):(t - 1)
   }
   
   # calculate counts outside marine reserve
   if (fished_areas_sampled == "far") {
     count_out <- Count[c(1, A), years, , fish, cr, nm]
-  } else {
-    count_out <- Count[y, years, , fish, cr, nm]
+  } else if (fished_areas_sampled == 'all') {
+    count_out <- Count[outside, years, , fish, cr, nm]
   }
  
   # calculate density ratio 
