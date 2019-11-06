cf8d0c4fb82255d5d8b1d253d6bdd1039ae09645
Victoria Quennessen <viquennessen@gmail.com>
2019-11-05 17:16:42 -0800

    added necessary info to run_plot_stuff.R

--- code/R/run_plot_stuff.R
+++ code/R/run_plot_stuff.R
@@ -1,9 +1,13 @@
 source('./plot_stuff.R')
 
-filepath1 = "../../data/1e2_NM_sims_yield.Rda"
-filepath2 = "../../data/1e2_NM_sims_biomass.Rda"
-filepath3 = "../../data/1e2_NM_sims_SSB.Rda"
-num_sims <- 1e2
+filepath1 = "../../data/1e4_NM_sims_yield.Rda"
+filepath2 = "../../data/1e4_NM_sims_biomass.Rda"
+filepath3 = "../../data/1e4_NM_sims_SSB.Rda"
+
+num_sims <- 1e4
+A <- 5
+time2 <- 20
+CR <- 6
 
 plot_stuff(filepath1, filepath2, filepath3, A, time2, CR, num_sims, 
            sample_size = num_sims, PD = 0.25)
