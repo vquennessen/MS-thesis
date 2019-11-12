fb48fc10609c76f2a949e88f3b992aa771ab06d0
Victoria Quennessen <viquennessen@gmail.com>
2019-11-12 08:13:49 -0800

    playing with new plotting color palette

--- code/R/run_plot_stuff.R
+++ code/R/run_plot_stuff.R
@@ -4,12 +4,12 @@ filepath1 = "../../data/1e4_NM_sims_yield.Rda"
 filepath2 = "../../data/1e4_NM_sims_biomass.Rda"
 filepath3 = "../../data/1e4_NM_sims_SSB.Rda"
 
-num_sims <- 1e4
+num_sims <- 10
 A <- 5
 time2 <- 20
 CR <- 6
 plot_individual_runs = T
-sample_size <- 5
+sample_size <- num_sims
 PD <- 0.25
 
 plot_stuff(filepath1, filepath2, filepath3, A, time2, CR, num_sims, 
