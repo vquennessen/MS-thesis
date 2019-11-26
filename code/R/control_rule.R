a429be894ece93c26918eca37160d3f9227f8bcf
Victoria Quennessen <viquennessen@gmail.com>
2019-11-26 09:03:20 -0800

    updated control rules to have final target DR of 0.8

--- code/R/control_rule.R
+++ code/R/control_rule.R
@@ -8,7 +8,7 @@ control_rule <- function(t, cr, nm, A, E, Count, time1, time2, transects,
                         fish_sampled = 'mature', transects)
     
     # calculate effort
-    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.6, 
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.8, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
@@ -19,7 +19,7 @@ control_rule <- function(t, cr, nm, A, E, Count, time1, time2, transects,
                         fish_sampled = 'mature', transects)
     
     # calculate effort
-    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.6, 
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.8, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
@@ -30,7 +30,7 @@ control_rule <- function(t, cr, nm, A, E, Count, time1, time2, transects,
                         fish_sampled = 'mature', transects)
     
     # calculate effort
-    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.6, 
+    E <- management(t, cr, E, DR, CR_type = 'DR', target_DR = 0.8, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
   
@@ -41,7 +41,7 @@ control_rule <- function(t, cr, nm, A, E, Count, time1, time2, transects,
                         fish_sampled = 'mature', transects)
     
     # calculate effort
-    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.6, nm)
+    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.8, nm)
     E <- management(t, cr, E, DR, CR_type = 'DR', target_DR, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
@@ -53,7 +53,7 @@ control_rule <- function(t, cr, nm, A, E, Count, time1, time2, transects,
                         fish_sampled = 'mature', transects)
     
     # calculate effort
-    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.6, nm)
+    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.8, nm)
     E <- management(t, cr, E, DR, CR_type = 'DR', target_DR, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
@@ -65,7 +65,7 @@ control_rule <- function(t, cr, nm, A, E, Count, time1, time2, transects,
                         fish_sampled = 'mature', transects)
     
     # calculate effort
-    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.6, nm)
+    target_DR <- transient_DR(nat_mortality, t, time1, time2, final_DR = 0.8, nm)
     E <- management(t, cr, E, DR, CR_type = 'DR', target_DR, 
                     floor_DR = 0.2, effort_inc_allowed = 0.1, time1)
   }
