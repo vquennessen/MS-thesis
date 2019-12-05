1f071ba95dddddcf4e223c3c66e2e25e52c47aba
Victoria Quennessen <viquennessen@gmail.com>
2019-12-05 14:31:19 -0800

    updated recruitment.R to have two potential recruitment modes - closed population dynamics or a pooled larval stage

--- code/R/recruitment.R
+++ code/R/recruitment.R
@@ -1,13 +1,17 @@
-recruitment = function(a, t, cr, nm, SSB, A, R0, h, B0, Eps, sigma_R, rec_age) {
+recruitment = function(a, t, cr, nm, SSB, A, R0, h, B0, Eps, sigma_R, rec_age, 
+                       recruitment_mode) {
   
   # Recruitment
   # Based on Babcock & MacCall (2011): Eq. (3)
   # Dimensions = 1 * 1
   
+  if (recruitment_mode == 'closed') { ssb <- SSB[a, t - rec_age, cr, nm]}
+  if (recruitment_mode == 'pool') { ssb <- sum(SSB[, t - rec_age, cr, nm] / A)}
+  
   adjR0 <- R0 / A
   adjB0 <- B0 / A
   
-  R1 <- (0.8 * adjR0 * h * SSB[a, t - rec_age, cr, nm]) / (0.2 * adjB0 * (1 - h) + (h - 0.2) * SSB[a, t - rec_age, cr, nm]) 
+  R1 <- (0.8 * adjR0 * h * ssb) / (0.2 * adjB0 * (1 - h) + (h - 0.2) * ssb) 
   R <- R1 * (exp(Eps[a, t, cr, nm] - sigma_R^2 / 2))
 
   return(R)
