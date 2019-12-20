3b0e87881418a461bf3f30ef3505d2af26c66a39
Victoria Quennessen <victoria.quennessen@oregonstate.edu>
2019-12-20 08:27:24 -0500

    finished entering CAB2005 parameters and started new entry for CAB2019 parameters in parameters.R

--- code/R/parameters.R
+++ code/R/parameters.R
@@ -52,6 +52,47 @@ parameters = function(species) {
                                             
   }
   
+  if (species == 'BR2015') {
+    # source: Cope et al. 2016
+    max_age <- 40                           # maximum age
+    M <- 0.17                               # natural mortality
+    rec_age <- 3                            # age at recruitment
+    af <- 2.6e-5; bf <- 2.88                # weight at length parameters (f)
+    a1f <- 1; L1f <- 20.32;                 # growth parameters (f)
+    a2f <- 40; L2f <- 49.67; Kf <- 0.21  
+    L50 <- 43.69                            # length at 50% maturity
+    k_mat <- -0.66                          # slope of maturity curve
+    ldp <- 0.1                              # larval drift proportion
+    h <- 0.77                               # steepness
+    phi <- 1.1                              # unfished recruits per spawner
+    sigma_R <- 0.5                          # recruitment standard deviation
+    rho_R <- 0                              # recruitment autocorrelation
+    AMP <- 0.1                              # adult movement proportion
+    D <- 0.604                              # depletion
+    Fb <- 0.02                            # fishing mortality to cause D 
+                                         ### TODO: find true value of Fb???
+    r <- 0.77                               # Proportion of positive transects 
+                                            #       in PISCO monitoring data
+    x <- 15.42                              # mean of positive transects
+    sp <- 16.97                             # std of positive transects
+    
+    c <- 0.2747                             # eggs produced per kg, intercept
+    b <- 0.0941                             # eggs produced per kg, slope
+    
+    #### fleets: sport, hook, trawl ####
+    fleets <- c('trawl', 'live', 'dead',    # names of fleets
+                'recO', 'recS')   
+    alpha <- c(0.325, 0.4, 0.35, 
+               0.65, 0.425)                 # slope of upcurve per fleet
+    beta <- c(0.25, 0.5, 0.4, 1.1, 0.5)     # slope of downcurve per fleet
+    F_fin <- c(0.325, 0.05, -0.11, 
+               -0.025, 0.135)               # final selectivity if dome-shaped 
+    A50_up <- c(7, 5, 5, 5, 3)              # L50 value for upcurve
+    A50_down <- c(15, 13, 13, 12, 6.5)      # L50 value for downcurve
+    cf <- c(0.0001, 0.1679, 0.0982,         # fraction of fishery caught / fleet
+            0.6979, 0.0358)               
+  } 
+  
   if (species == 'CAB2005') {
     # Black Rockfish 2003 assessment
     # Source: Ralston & Dick 2003
@@ -93,46 +134,45 @@ parameters = function(species) {
 
   }
   
-  if (species == 'BR2015') {
-    # source: Cope et al. 2016
-    max_age <- 40                           # maximum age
-    M <- 0.17                               # natural mortality
-    rec_age <- 3                            # age at recruitment
-    af <- 2.6e-5; bf <- 2.88                # weight at length parameters (f)
-    a1f <- 1; L1f <- 20.32;                 # growth parameters (f)
-    a2f <- 40; L2f <- 49.67; Kf <- 0.21  
-    L50 <- 43.69                            # length at 50% maturity
-    k_mat <- -0.66                          # slope of maturity curve
-    ldp <- 0.1                              # larval drift proportion
-    h <- 0.77                               # steepness
-    phi <- 1.1                              # unfished recruits per spawner
-    sigma_R <- 0.5                          # recruitment standard deviation
-    rho_R <- 0                              # recruitment autocorrelation
-    AMP <- 0.1                              # adult movement proportion
-    D <- 0.604                              # depletion
-    Fb <- 0.02                            # fishing mortality to cause D 
-                                         ### TODO: find true value of Fb???
-    r <- 0.77                               # Proportion of positive transects 
+  if (species == 'CAB2019') {
+    # Black Rockfish 2003 assessment
+    # Source: Ralston & Dick 2003
+    max_age <-                            # maximum age
+    M <-                               # natural mortality
+    rec_age <-                             # age at recruitment
+    af <- ; bf <-               # weight at length parameters (f)
+    a1f <- ; L1f <- ;                  # growth parameters (f)
+    a2f <- ; L2f <- ; Kf <-   
+    L50 <-                            # length at 50% maturity
+    k_mat <-                          # slope of maturity curve
+    ldp <-                               # larval drift proportion
+    h <-                                 # steepness
+    phi <-                              # unfished recruits per spawner
+    sigma_R <-                             # recruitment standard deviation
+    rho_R <-                               # recruitment autocorrelation
+    AMP <-                               # adult movement proportion
+    D <-                                 # depletion
+    Fb <-                                # fishing mortality to cause D
+    r <-                                # Proportion of positive transects 
                                             #       in PISCO monitoring data
-    x <- 15.42                              # mean of positive transects
-    sp <- 16.97                             # std of positive transects
+    x <-                                # mean of positive transects
+    sp <-                               # std of positive transects
     
-    c <- 0.2747                             # eggs produced per kg, intercept
-    b <- 0.0941                             # eggs produced per kg, slope
+    c <- 0.0273                             # eggs produced per kg, intercept
+    b <- 1.53e-4                            # eggs produced per kg, slope
     
     #### fleets: sport, hook, trawl ####
-    fleets <- c('trawl', 'live', 'dead',    # names of fleets
-                'recO', 'recS')   
-    alpha <- c(0.325, 0.4, 0.35, 
-               0.65, 0.425)                 # slope of upcurve per fleet
-    beta <- c(0.25, 0.5, 0.4, 1.1, 0.5)     # slope of downcurve per fleet
-    F_fin <- c(0.325, 0.05, -0.11, 
-               -0.025, 0.135)               # final selectivity if dome-shaped 
-    A50_up <- c(7, 5, 5, 5, 3)              # L50 value for upcurve
-    A50_down <- c(15, 13, 13, 12, 6.5)      # L50 value for downcurve
-    cf <- c(0.0001, 0.1679, 0.0982,         # fraction of fishery caught / fleet
-            0.6979, 0.0358)               
+    fleets <- c('dead', 'live', 'man.made', 
+                'shore', 'PBR', 'CPFV')     # names of fleets
+    alpha <- c()                  # slope of upcurve per fleet
+    beta <- c()     # slope of downcurve per fleet
+    F_fin <- c()    # final selectivity if dome-shaped 
+    A50_up <- c()           # A50 value for upcurve
+    A50_down <- c()        # A50 value for downcurve
+    cf <- c()                 # fraction of fishery caught / fleet
+    
   }
+  
   
   output = list(max_age, M, rec_age, af, bf, a1f, L1f, a2f, L2f, Kf, L50, k_mat, 
                 ldp, h, phi, sigma_R, rho_R, AMP, D, Fb, r, x, sp, c, b, fleets, 
