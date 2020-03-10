a1459c432f839a134142de54b26655dc49b66c36
Victoria Quennessen <viquennessen@gmail.com>
2020-03-10 11:57:12 -0700

    scripts to optimize running cluster.R on the cluster

---
+++ code/cluster_runs_02.R
@@ -0,0 +1,9 @@
+remotes::install_github("vquennessen/densityratio")
+library(densityratio)
+source('./cluster.R')
+
+Species = 'BR_OR_2015'
+Final_DR <- 0.2
+num_sims <- 1000
+
+cluster(Species, Final_DR, num_sims)
