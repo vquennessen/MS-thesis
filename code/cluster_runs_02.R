1240bb3fa6d840275458ba8be1a0d2fae90c545c
Victoria Quennessen <viquennessen@gmail.com>
2020-03-18 16:31:43 -0700

    deleted cluster_runs variations

--- code/cluster_runs_02.R
+++
@@ -1,9 +0,0 @@
-remotes::install_github("vquennessen/densityratio")
-library(densityratio)
-source('./cluster.R')
-
-Species = 'BR_OR_2015'
-Final_DR <- 0.2
-num_sims <- 1000
-
-cluster(Species, Final_DR, num_sims)
--- code/cluster_runs_10.R
+++
@@ -1,9 +0,0 @@
-remotes::install_github("vquennessen/densityratio")
-library(densityratio)
-source('./cluster.R')
-
-Species = 'BR_OR_2015'
-Final_DR <- 1.0
-num_sims <- 1000
-
-cluster(Species, Final_DR, num_sims)
