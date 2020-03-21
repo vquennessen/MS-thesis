c578896c349f9bc84b92accf5835be890473b4e5
Victoria Quennessen <viquennessen@gmail.com>
2020-03-20 22:32:34 -0700

    updaing cluster_runs.R

--- code/cluster_runs.R
+++ code/cluster_runs.R
@@ -1,10 +1,40 @@
-source('./cluster.R')
+library(remotes)
+remotes::install_github('vquennessen/densityratio')
+library(densityratio)
+source('./run_base_model.R')
 
-final_DRs <- seq(0.2, 1, by = 0.2)
-sims <- 10000
+Species = c('BR_OR_2015', 'CAB_OR_2019', 'LING_OW_2017', 'CR_OR_2015')
+num_sims = 1e1
 
-for (i in 1:length(final_DRs)) {
+function2 <- function(Species, num_sims) {
   
-  cluster(Species = 'BR_OR_2015', Final_DR = final_DRs[i], num_sims = sims)
+  
+  
+  arrays <- lapply(Species, run_base_model)
+  
+  sims_N       <- arrays[[1]][[1]]
+  sims_biomass <- arrays[[1]][[2]]
+  sims_SSB     <- arrays[[1]][[3]]
+  sims_yield   <- arrays[[1]][[4]]
+  sims_effort  <- arrays[[1]][[5]]
+  sims_DR      <- arrays[[1]][[6]]
+  
+  Q <- ifelse(num_sims < 1000, num_sims,  paste("1e", log10(num_sims), sep = ''))
+  
+  filepath1 = paste('../data/', Species, '/', Q, '_N.Rda', sep = '')
+  filepath2 = paste('../data/', Species, '/', Q, "_biomass.Rda", sep = '')
+  filepath3 = paste('../data/', Species, '/', Q, "_SSB.Rda", sep = '')
+  filepath4 = paste('../data/', Species, '/', Q, "_yield.Rda", sep = '')
+  filepath5 = paste('../data/', Species, '/', Q, "_effort.Rda", sep = '')
+  filepath6 = paste('../data/', Species, '/', Q, "_DR.Rda", sep = '')
+  
+  save(sims_N, file = filepath1)
+  save(sims_biomass, file = filepath2)
+  save(sims_SSB, file = filepath3)
+  save(sims_yield, file = filepath4)
+  save(sims_effort, file = filepath5)
+  save(sims_DR, file = filepath6)
   
 }
+
+lapply('BR_OR_2015', function2, num_sims)
