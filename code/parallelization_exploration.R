a09c8be660985cffd04d7f6a7ef5ed233181ddc4
Victoria Quennessen <viquennessen@gmail.com>
2020-03-20 22:31:55 -0700

    updated paralellization_exploration.R

--- code/parallelization_exploration.R
+++ code/parallelization_exploration.R
@@ -8,16 +8,21 @@ devtools::install_git('https://github.com/vquennessen/densityratio.git')
 library(densityratio)
 source('try1.R')
 
+num_sims = 4
+
+system.time(
+  lapply(num_sims, try1)
+)
+
 num_sims = 20
 
 system.time(
   lapply(num_sims, try1)
 )
- 
-### time on my laptop (4 sims): 6440.07, 6760.33 seconds
-### time on my laptop (20 sims): 18510 seconds
-### time on the cluster (4 sims): 3113.632, 2548.881 seconds
-### time on the cluster (20 sims): 11522.746,  seconds
+
+system.time(
+  lapply(num_sims, try1)
+)
 
 ###### using mclapply() to run sims in parallel? #####
 
@@ -39,5 +44,5 @@ system.time(
 ### time on cluster with 4 cores (4 sims): 3487.170, 3362.931
 ### time on cluster with 12 cores (4 sims): 2742.638, 3176.494
 
-### time on cluster with 4 cores (20 sims): 
+### time on cluster with 4 cores (20 sims): 11977.96
 ### time on cluster with 12 cores (20 sims): 
