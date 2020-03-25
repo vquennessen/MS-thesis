9f6df8c1babe0698aaf3d77648bc61c63e7ce46d
Victoria Quennessen <viquennessen@gmail.com>
2020-03-25 14:24:33 -0700

    edited cluster_runs to use lapply_base_model() instead of run_base_model()

--- code/cluster_runs.R
+++ code/cluster_runs.R
@@ -2,13 +2,17 @@ library(remotes)
 remotes::install_github('vquennessen/densityratio')
 library(densityratio)
 library(parallel)
-source('./run_base_model.R')
+source('./lapply_base_model.R')
 
 species_list = c('BR_OR_2015', 'CAB_OR_2019', 'LING_OW_2017', 'CR_OR_2015')
 sims = 1e4
 
+# time <- system.time(
+#   mclapply(species_list, run_base_model, mc.cores = 12, num_sims = sims)
+# )
+
 time <- system.time(
-  mclapply(species_list, run_base_model, mc.cores = 12, num_sims = sims)
+  mclapply(species_list, lapply_base_model, mc.cores = 12, num_sims = sims)
 )
 
 write(time, file = 'mclapply_results.txt')
