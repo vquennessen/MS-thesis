121dba61df755c0321ad0af411cb765fe9bfdb28
Victoria Quennessen <viquennessen@gmail.com>
2020-04-14 21:20:45 -0700

    cluster_runs to figure out timing thing

--- code/cluster_runs.R
+++ code/cluster_runs.R
@@ -4,9 +4,22 @@ library(densityratio)
 library(parallel)
 source('run_base_model.R')
 
-# species_list = c('BR_OR_2015', 'CAB_OR_2019', 'LING_OW_2017', 'CR_OR_2015')
+species_list = c('BR_OR_2015', 'CAB_OR_2019', 'LING_OW_2017', 'CR_OR_2015')
 
-sims = 2
-# mclapply(species_list, run_base_model, mc.cores = 12, num_sims = sims)
-# lapply(species_list, run_base_model, num_sims = sims)
-lapply('CAB_OR_2019', run_base_model, num_sims = sims)
+sims = 40
+mclapply(species_list, run_base_model, mc.cores = 12, num_sims = sims)
+
+sims1 = 30
+sims2 = 29
+
+mclapply('CAB_OR_2019', run_base_model, mc.cores = 12, num_sims = sims1)
+lapply('CAB_OR_2019', run_base_model, num_sims = sims2)
+
+mclapply('LING_OW_2017', run_base_model, mc.cores = 12, num_sims = sims1)
+lapply('LING_OW_2017', run_base_model, num_sims = sims2)
+
+mclapply('BR_OR_2015', run_base_model, mc.cores = 12, num_sims = sims1)
+lapply('BR_OR_2015', run_base_model, num_sims = sims2)
+
+mclapply('CR_OR_2015', run_base_model, mc.cores = 12, num_sims = sims1)
+lapply('CR_OR_2015', run_base_model, num_sims = sims2)
