cab350f7d1afc90aac49acdfa596c626a7c94c8c
Victoria Quennessen <viquennessen@gmail.com>
2020-04-22 12:30:07 -0700

    edited cluster_runs.R

--- code/cluster_runs.R
+++ code/cluster_runs.R
@@ -6,20 +6,19 @@ source('run_base_model.R')
 
 species_list = c('BR_OR_2015', 'CAB_OR_2019', 'LING_OW_2017', 'CR_OR_2015')
 
-sims = 40
-mclapply(species_list, run_base_model, mc.cores = 12, num_sims = sims)
+mclapply(species_list, run_base_model, mc.cores = 12, num_sims = 72)
 
-sims1 = 30
-sims2 = 29
+lapply('CR_OR_2015', run_base_model, num_sims = 52)
+lapply('CR_OR_2015', run_base_model, num_sims = 100)
+lapply('CR_OR_2015', run_base_model, num_sims = 100)
 
-mclapply('CAB_OR_2019', run_base_model, mc.cores = 12, num_sims = sims1)
-lapply('CAB_OR_2019', run_base_model, num_sims = sims2)
-
-mclapply('LING_OW_2017', run_base_model, mc.cores = 12, num_sims = sims1)
-lapply('LING_OW_2017', run_base_model, num_sims = sims2)
-
-mclapply('BR_OR_2015', run_base_model, mc.cores = 12, num_sims = sims1)
-lapply('BR_OR_2015', run_base_model, num_sims = sims2)
-
-mclapply('CR_OR_2015', run_base_model, mc.cores = 12, num_sims = sims1)
-lapply('CR_OR_2015', run_base_model, num_sims = sims2)
+lapply('CAB_OR_2019', run_base_model, num_sims = 95)
+lapply('CAB_OR_2019', run_base_model, num_sims = 96)
+lapply('CAB_OR_2019', run_base_model, num_sims = 97)
+lapply('CAB_OR_2019', run_base_model, num_sims = 98)
+lapply('CAB_OR_2019', run_base_model, num_sims = 99)
+lapply('CAB_OR_2019', run_base_model, num_sims = 101)
+lapply('CAB_OR_2019', run_base_model, num_sims = 102)
+lapply('CAB_OR_2019', run_base_model, num_sims = 103)
+lapply('CAB_OR_2019', run_base_model, num_sims = 104)
+lapply('CAB_OR_2019', run_base_model, num_sims = 105)
