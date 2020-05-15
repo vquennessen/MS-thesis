bd47c576252bbbba41ef5afe21f9efed89520f23
Victoria Quennessen <viquennessen@gmail.com>
2020-05-15 10:36:17 -0700

    file to store params for troubleshooting densityratio

---
+++ code/troubleshooting_params.R
@@ -0,0 +1,36 @@
+Species = 'BR_OR_2015'
+
+Final_DRs <- c(0.4, 0.6, 0.8)
+
+Sampling_Error = FALSE
+Transects = 24
+Stochasticity = FALSE
+M_Error = 0.05
+Control_rules = 1:6
+Output.N = TRUE
+Output.Abundance = TRUE
+Output.Biomass = TRUE
+Output.SSB = TRUE
+Output.Yield = TRUE
+Output.Effort = TRUE
+Output.Density.Ratio = TRUE
+
+# set arguments
+R0 = 1e+5
+A = 5
+MPA = 3
+Time1 = 50
+Time2 = 20
+Recruitment_mode = 'pool'
+Surveys = TRUE
+Fishery_management = TRUE
+Fishing = TRUE
+Adult_movement = FALSE
+Plotting = FALSE
+Years_sampled = 1
+Areas_sampled = 'all'
+Ind_sampled = 'all'
+Floor_DR = 0.2
+Allocation = 'IFD'
+BM = FALSE
+LDP = 0
