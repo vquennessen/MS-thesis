a82805e8909d4a0a6de99abb05bed942a7332002
Victoria Quennessen <viquennessen@gmail.com>
2020-05-15 10:38:41 -0700

    updated cluster_runs to incorporate scenario

--- code/cluster_runs.R
+++ code/cluster_runs.R
@@ -6,4 +6,12 @@ source('run_base_model.R')
 
 species_list = c('BR_OR_2015', 'CAB_OR_2019', 'LING_OW_2017', 'CR_OR_2015')
 
-mclapply(species_list, run_base_model, mc.cores = 12, num_sims = 10)
+Scenario = 'Both'
+
+Final_DRs <- c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+
+mclapply(species_list, run_base_model, mc.cores = 12, num_sims = 2500,
+         Scenario, Final_DRs)
+
+mclapply(species_list, run_base_model, mc.cores = 12, num_sims = 2500,
+         Scenario, Final_DRs)
