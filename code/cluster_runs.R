176ad781c6d2099934c38bab61922160ffbd6dbb
Victoria Quennessen <viquennessen@gmail.com>
2020-05-25 14:26:02 -0700

    updated cluster_runs

--- code/cluster_runs.R
+++ code/cluster_runs.R
@@ -10,8 +10,17 @@ Scenario = 'Both'
 
 Final_DRs <- c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 
-mclapply(species_list, run_base_model, mc.cores = 12, num_sims = 2500,
+mclapply(species_list, run_base_model, mc.cores = 12, num_sims = 101,
          Scenario, Final_DRs)
 
-mclapply(species_list, run_base_model, mc.cores = 12, num_sims = 2500,
+mclapply(species_list, run_base_model, mc.cores = 12, num_sims = 102,
+         Scenario, Final_DRs)
+
+mclapply(species_list, run_base_model, mc.cores = 12, num_sims = 103,
+         Scenario, Final_DRs)
+
+mclapply(species_list, run_base_model, mc.cores = 12, num_sims = 104,
+         Scenario, Final_DRs)
+
+mclapply(species_list, run_base_model, mc.cores = 12, num_sims = 106,
          Scenario, Final_DRs)
