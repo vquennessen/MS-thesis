8ef6882934b4e24c13761cb2a3c6e6a2cefd0033
Victoria Quennessen <viquennessen@gmail.com>
2020-05-25 14:24:23 -0700

    updated run_base_model given updates in densityratio

--- code/run_base_model.R
+++ code/run_base_model.R
@@ -8,109 +8,61 @@ run_base_model <- function(Species, num_sims, Scenario, Final_DRs) {
                       num_sims, ' sims', sep = '')
   write(start_time, file = 'progress.txt', append = TRUE)
   
-  if (Scenario == 'None') {
+  if (Scenario == 'None' | Scenario == 'Test') {
     Sampling_Error = FALSE
     Transects = 24
     Stochasticity = FALSE
-    Output.N = TRUE
-    Output.Abundance = TRUE
-    Output.Biomass = TRUE
-    Output.SSB = TRUE
-    Output.Yield = TRUE
-    Output.Effort = TRUE
-    Output.Density.Ratio = TRUE
     
   } else if (Scenario == 'Sampling.LowT') {
     Sampling_Error = TRUE
     Transects = 12
     Stochasticity = FALSE
-    Output.N = TRUE
-    Output.Abundance = FALSE
-    Output.Biomass = TRUE
-    Output.SSB = FALSE
-    Output.Yield = TRUE
-    Output.Effort = FALSE
-    Output.Density.Ratio = FALSE
     
   } else if (Scenario == 'Sampling') {
     Sampling_Error = TRUE
     Transects = 24
     Stochasticity = FALSE
-    Output.N = TRUE
-    Output.Abundance = FALSE
-    Output.Biomass = TRUE
-    Output.SSB = FALSE
-    Output.Yield = TRUE
-    Output.Effort = FALSE
-    Output.Density.Ratio = FALSE
     
   } else if (Scenario == 'Sampling.HighT') {
     Sampling_Error = TRUE
     Transects = 36
     Stochasticity = FALSE
-    Output.N = TRUE
-    Output.Abundance = FALSE
-    Output.Biomass = TRUE
-    Output.SSB = FALSE
-    Output.Yield = TRUE
-    Output.Effort = FALSE
-    Output.Density.Ratio = FALSE
     
   } else if (Scenario == 'Recruitment') {
     Sampling_Error = FALSE
     Transects = 24
     Stochasticity = TRUE
-    Output.N = TRUE
-    Output.Abundance = FALSE
-    Output.Biomass = TRUE
-    Output.SSB = FALSE
-    Output.Yield = TRUE
-    Output.Effort = FALSE
-    Output.Density.Ratio = FALSE
     
   } else if (Scenario == 'Both.LowT') {
     Sampling_Error = TRUE
     Transects = 12
     Stochasticity = TRUE
-    Output.N = TRUE
-    Output.Abundance = FALSE
-    Output.Biomass = TRUE
-    Output.SSB = FALSE
-    Output.Yield = TRUE
-    Output.Effort = FALSE
-    Output.Density.Ratio = FALSE
     
   } else if (Scenario == 'Both') {
     Sampling_Error = TRUE
     Transects = 24
     Stochasticity = TRUE
-    Output.N = TRUE
-    Output.Abundance = FALSE
-    Output.Biomass = TRUE
-    Output.SSB = FALSE
-    Output.Yield = TRUE
-    Output.Effort = FALSE
-    Output.Density.Ratio = FALSE
     
   } else if (Scenario == 'Both.HighT') {
     Sampling_Error = TRUE
     Transects = 36
     Stochasticity = TRUE
-    Output.N = TRUE
-    Output.Abundance = FALSE
-    Output.Biomass = TRUE
-    Output.SSB = FALSE
-    Output.Yield = TRUE
-    Output.Effort = FALSE
-    Output.Density.Ratio = FALSE
   }
   
   # set arguments
+  Output.N = TRUE
+  Output.Abundance = TRUE
+  Output.Biomass = TRUE
+  Output.SSB = TRUE
+  Output.Yield = TRUE
+  Output.Effort = TRUE
+  Output.Density.Ratio = TRUE
   R0 = 1e+5
   A = 5
   MPA = 3
   Time1 = 50
   Time2 = 20
+  TimeT = Time1 + Time2
   Recruitment_mode = 'pool'
   Surveys = TRUE
   Fishery_management = TRUE
@@ -124,13 +76,11 @@ run_base_model <- function(Species, num_sims, Scenario, Final_DRs) {
   Allocation = 'IFD'
   BM = FALSE
   LDP = 0.1
-  Control_rules = 1:6
   M_Error = 0.05
   
   # dimensions
-  TimeT <- Time1 + Time2
-  CR <- length(Control_rules)
-  NM <- 3
+  CR <- 6
+  NM <- 2
   FDR <- length(Final_DRs)
   Rec_age <- parameters(Species)[[3]]
   Max_age <- parameters(Species)[[1]]
@@ -142,19 +92,16 @@ run_base_model <- function(Species, num_sims, Scenario, Final_DRs) {
                   c(n, A, Time2 + 1, CR, FDR, num_sims))
   sims_biomass <- array(rep(0, A*(Time2 + 1)*CR*FDR*num_sims),
                         c(A, Time2 + 1, CR, FDR, num_sims))
-  sims_yield <- array(rep(0, A*(Time2 + 1)*CR*FDR*num_sims),
-                      c(A, Time2 + 1, CR, FDR, num_sims))
-  
-  if (Scenario == 'None') {
-    sims_SSB <- array(rep(0, A*(Time2 + 1)*CR*FDR*num_sims),
-                      c(A, Time2 + 1, CR, FDR, num_sims))
-    sims_effort <- array(rep(0, TimeT*CR*FDR*num_sims),
-                         c(TimeT, CR, FDR, num_sims))
-    sims_DR <- array(rep(0, TimeT*CR*FDR*num_sims),
-                     c(TimeT, CR, FDR, num_sims))
-    sims_abundance <- array(rep(0, A*(Time2 + 1)*CR*FDR*num_sims),
-                            c(A, Time2 + 1, CR, FDR, num_sims))
-  }
+  sims_yield <- array(rep(0, (Time2 + 1)*CR*FDR*num_sims),
+                      c(Time2 + 1, CR, FDR, num_sims))
+  sims_DR <- array(rep(0, (Time2 + 1)*CR*FDR*num_sims),
+                   c(Time2 + 1, CR, FDR, num_sims))
+  sims_SSB <- array(rep(0, A*(Time2 + 1)*CR*FDR*num_sims),
+                    c(A, Time2 + 1, CR, FDR, num_sims))
+  sims_effort <- array(rep(0, TimeT*CR*FDR*num_sims),
+                       c(TimeT, CR, FDR, num_sims))
+  sims_abundance <- array(rep(0, A*(Time2 + 1)*CR*FDR*num_sims),
+                          c(A, Time2 + 1, CR, FDR, num_sims))
   
   # run the model for each simulation
   for (i in 1:num_sims) {
@@ -164,22 +111,18 @@ run_base_model <- function(Species, num_sims, Scenario, Final_DRs) {
                          Fishery_management, Fishing, Transects, Adult_movement, 
                          Plotting, Final_DRs, Years_sampled, Areas_sampled, 
                          Ind_sampled, Floor_DR, Allocation, BM, LDP, 
-                         Control_rules, Output.N, Output.Abundance, 
-                         Output.Biomass, Output.SSB, Output.Yield, 
-                         Output.Effort, Output.Density.Ratio)
+                         Output.N, Output.Abundance, Output.Biomass, Output.SSB, 
+                         Output.Yield, Output.Effort, Output.Density.Ratio)
     
     # save the relative yield and biomasses for all areas, times after reserve
     # implementation, control rules, and final density ratios
     sims_N[, , , , , i]       <- output$N
     sims_biomass[, , , , i]   <- output$Biomass
-    sims_yield[, , , , i]     <- output$Yield
-    
-    if (Scenario == 'None') {
-      sims_SSB[, , , , i]       <- output$SSB
-      sims_effort[, , , i]      <- output$Effort
-      sims_DR[, , , i]            <- output$Density_ratio
-      sims_abundance[, , , , i] <- output$Abundance
-    }
+    sims_yield[, , , i]       <- output$Yield
+    sims_DR[, , , i]          <- output$Density_ratio
+    sims_SSB[, , , , i]       <- output$SSB
+    sims_effort[, , , i]      <- output$Effort
+    sims_abundance[, , , , i] <- output$Abundance
     
     if (i %% (num_sims/10) == 0) {
       update <- paste(Sys.time(), ' - ', Species, ' - ', i/num_sims*100, 
@@ -195,23 +138,17 @@ run_base_model <- function(Species, num_sims, Scenario, Final_DRs) {
   filepath1 = paste('../data/', Scenario, '/', Species, '/', Q, '_N.Rda', sep = '')
   filepath2 = paste('../data/', Scenario, '/', Species, '/', Q, '_biomass.Rda', sep = '')
   filepath3 = paste('../data/', Scenario, '/', Species, '/', Q, '_yield.Rda', sep = '')
-  
-  if (Scenario == 'None') {
-    filepath4 = paste('../data/', Scenario, '/', Species, '/', Q, '_SSB.Rda', sep = '')
-    filepath5 = paste('../data/', Scenario, '/', Species, '/', Q, '_effort.Rda', sep = '')
-    filepath6 = paste('../data/', Scenario, '/', Species, '/', Q, '_DR.Rda', sep = '')
-    filepath7 = paste('../data/', Scenario, '/', Species, '/', Q, '_abundance.Rda', sep = '')
-  }
+  filepath4 = paste('../data/', Scenario, '/', Species, '/', Q, '_DR.Rda', sep = '')
+  filepath5 = paste('../data/', Scenario, '/', Species, '/', Q, '_effort.Rda', sep = '')
+  filepath6 = paste('../data/', Scenario, '/', Species, '/', Q, '_SSB.Rda', sep = '')
+  filepath7 = paste('../data/', Scenario, '/', Species, '/', Q, '_abundance.Rda', sep = '')
   
   save(sims_N, file = filepath1)
   save(sims_biomass, file = filepath2)
   save(sims_yield, file = filepath3)
-  
-  if (Scenario == 'None') {
-    save(sims_SSB, file = filepath4)
-    save(sims_effort, file = filepath5)
-    save(sims_DR, file = filepath6)
-    save(sims_abundance, file = filepath7)
-  }
+  save(sims_DR, file = filepath4)
+  save(sims_effort, file = filepath5)
+  save(sims_SSB, file = filepath6)
+  save(sims_abundance, file = filepath7)
   
 }
