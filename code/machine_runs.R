dd70a7cabb93655c170bbec8dd3b6a8d27f01463
Victoria Quennessen <viquennessen@gmail.com>
2020-06-19 11:25:48 -0700

    edited machine_runs

--- code/machine_runs.R
+++ code/machine_runs.R
@@ -4,23 +4,25 @@ library(densityratio)
 library(parallel)
 source('run_base_model.R')
 
-Scenario = 'Both'
+Scenario = 'Both.LowT'
+species_list_1 = c('CR_OR_2015', 'LING_OW_2017', 'BR_OR_2015')
+# species_list_1 = c('CR_OR_2015', 'BR_OR_2015')
 
-species_list_1 = c('BR_OR_2015', 'LING_OW_2017', 'CR_OR_2015')
 species_list_2 = c('CAB_OR_2019')
 
 Final_DRs_1 <- c(0.6, 0.7, 0.8, 0.9)
 Final_DRs_2 <- c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 
-lapply(species_list_1, run_base_model, num_sims = 3, Scenario, Final_DRs_1)
-lapply(species_list_2, run_base_model, num_sims = 3, Scenario, Final_DRs_2)
+lapply(species_list_1, run_base_model, num_sims = 25, Scenario, Final_DRs_1)
+lapply(species_list_2, run_base_model, num_sims = 25, Scenario, Final_DRs_2)
 
 # lapply(species_list_1, run_base_model, num_sims = 10, Scenario, Final_DRs_1)
 # lapply(species_list_2, run_base_model, num_sims = 10, Scenario, Final_DRs_2)
 # 
 # lapply(species_list_1, run_base_model, num_sims = 10, Scenario, Final_DRs_1)
 # lapply(species_list_2, run_base_model, num_sims = 10, Scenario, Final_DRs_2)
-# 
+
+
 # lapply(species_list_1, run_base_model, num_sims = 10, Scenario, Final_DRs_1)
 # lapply(species_list_2, run_base_model, num_sims = 10, Scenario, Final_DRs_2)
 # 
