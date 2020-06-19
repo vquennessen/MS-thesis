df2f642d0cfc24ee4f9c5a0e978978a440e67774
Victoria Quennessen <viquennessen@gmail.com>
2020-06-19 11:44:29 -0700

    started script to quantify varibility in DR using CA blue rockfish data

---
+++ code/variability_in_DR.R
@@ -0,0 +1,6 @@
+# calculate variability in density ratio calculations
+
+# read in Blue Rockfish data
+BR <- read.csv('C:/Users/Vic/Box/Quennessen_Thesis/data/PISCO_taxon_lookup_table.csv', 
+               header = TRUE)
+
