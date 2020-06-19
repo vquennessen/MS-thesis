305dd0a43a79a039cac00a7a2a75bea8a2414da3
Victoria Quennessen <viquennessen@gmail.com>
2020-06-19 11:49:57 -0700

    edited and renamed code to plot relative biomass (with M)

--- code/plot_relative_biomass.R
+++ code/plot_relative_biomass.R
@@ -9,62 +9,85 @@ library(densityratio)
 
 ###############################################################################
 # CHECK THESE EVERY TIME
-num_sims <- 2
-data_folder <- 'Test_all'
-figures_folder <- 'Test_all/relative'
+num_sims <- 6193
+data_folder <- 'Both'
+figures_folder <- 'Both/relative'
+cluster <- TRUE
+png_width <- 6
+png_height <- 5
+y1 <- 0.25
+y2 <- 1.5
+y1.1 <- 0.95
+y2.1 <- 2
 ###############################################################################
 
 # species to compare
-species_list <- c('BR_OR_2015', 'CAB_OR_2019', 'LING_OW_2017', 'CR_OR_2015')
-Names <- c('Black Rockfish', 'Cabezon', 'Lingcod', 'Canary Rockfish')
+species_list <- c('CR_OR_2015', 'BR_OR_2015', 'LING_OW_2017', 'CAB_OR_2019')
+Names <- c('Canary Rockfish', 'Black Rockfish', 'Lingcod', 'Cabezon')
 
 # set variables
 A = 5
 MPA = 3
 Time1 = 50
 Time2 = 20
-Final_DRs = c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+Final_DRs1 <- c(0.6, 0.7, 0.8, 0.9)
+Final_DRs2 <- c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 Control_rules = c(1:6)
+types <- c('Static', 'Transient')
 
 # dimensions
-TimeT <- Time1 + Time2
-CR <- length(Control_rules)
-FDR <- length(Final_DRs)
 sample_size = num_sims
 PD = 0.25
 plot_individual_runs = FALSE
 Error = 0.05
 estimates <- c('True', 'Low', 'High')
-ENM = 2
 
 nT <- Time2 + 1
 nE <- length(estimates)
 nC <- length(Control_rules)
 nS <- length(species_list)
-nF <- length(Final_DRs)
+nF1 <- length(Final_DRs1)
+nF2 <- length(Final_DRs2)
+nFall <- nF2 + 3*nF1
 
-base <- data.frame(Type = rep(c('Static', 'Transient'), each = nE*nF*nT),
-                   Estimate = rep(estimates, times = 2, each = nF*nT),
-                   FDR = rep(Final_DRs, each = nT, times = 2*nE), 
-                   Time = rep(0:Time2, times = 2*nE*nF),
-                   Value = rep(0, 2*nE*nT*nF), 
-                   Lower = rep(0, 2*nE*nT*nF), 
-                   Upper = rep(0, 2*nE*nT*nF))
+base1 <- data.frame(Estimate = rep(estimates, each = 2*nF1*nT),
+                    Type = rep(types, each = nF1*nT, times = nE), 
+                    FDR = rep(Final_DRs1, each = nT, times = 2*nE), 
+                    Time = rep(0:Time2, times = 2*nF1*nE), 
+                    Value = rep(0, 2*nE*nF1*nT), 
+                    Lower = rep(0, 2*nE*nF1*nT),
+                    Upper = rep(0, 2*nE*nF1*nT))
+
+base2 <- data.frame(Estimate = rep(estimates, each = 2*nF2*nT),
+                    Type = rep(types, each = nF2*nT, times = nE), 
+                    FDR = rep(Final_DRs2, each = nT, times = 2*nE), 
+                    Time = rep(0:Time2, times = 2*nF2*nE), 
+                    Value = rep(0, 2*nE*nF2*nT), 
+                    Lower = rep(0, 2*nE*nF2*nT),
+                    Upper = rep(0, 2*nE*nF2*nT))
 
 for (s in 1:length(species_list)) {
-  
+    
   # load objects
+  if (cluster == TRUE) {
+    load(paste('~/Documents/MS-thesis/data/', data_folder, '/', 
+               species_list[s], '/', num_sims, '_biomass.Rda', sep = ''))
+  } else {
   load(paste('~/Projects/MS-thesis/data/', data_folder, '/', 
              species_list[s], '/', num_sims, '_biomass.Rda', sep = ''))
+  }
   
   Rec_age <- parameters(species_list[s])[[3]]
   Max_age <- parameters(species_list[s])[[1]]
   ages <- Rec_age:Max_age
   n <- length(ages)
   M <- parameters(species_list[s])[[2]]
-
+  
   # sample from simulations
   indices <- sample(1:num_sims, sample_size, replace = FALSE)
+  
+  # set nF value for species 
+  nF <- ifelse(s == 4, nF2, nF1)  
   
   ##### relative biomass and median, upper, and lower limits  #####
   
@@ -103,99 +126,85 @@ for (s in 1:length(species_list)) {
     }
   } 
   
-  ##### far - initialize and fill in data frame #####
+  ##### fill in data frame #####
   
   # make copies of base data frame called far, near, and inside
-  far <- base; near <- base; inside <- base
+  if (s != 4) {far <- base1; near <- base1; inside <- base1
+  } else { far <- base2; near <- base2; inside <- base2 }
   
-  for (type in 1:2) {
-    for (e in 1:nE) {
+  for (e in 1:nE) { 
+    for (type in 1:2) {
       for (fdr in 1:nF) {
         for (t in 1:nT) {
-          index <- (type - 1)*nE*nF*nT + (e - 1)*nF*nT + (fdr - 1)*nT + t
-          far$Value[index] <- B_medians[1, t, 2*e - type %% 2, fdr]
-          far$Lower[index] <- B_lower[1, t, 2*e - type %% 2, fdr]
-          far$Upper[index] <- B_upper[1, t, 2*e - type %% 2, fdr]
-          near$Value[index] <- B_medians[2, t, 2*e - type %% 2, fdr]
-          near$Lower[index] <- B_lower[2, t, 2*e - type %% 2, fdr]
-          near$Upper[index] <- B_upper[2, t, 2*e - type %% 2, fdr]
-          inside$Value[index] <- B_medians[3, t, 2*e - type %% 2, fdr]
-          inside$Lower[index] <- B_lower[3, t, 2*e - type %% 2, fdr]
-          inside$Upper[index] <- B_upper[3, t, 2*e - type %% 2, fdr]
+          
+          cr <- 2*e - type %% 2
+          
+          index <- (e - 1)*2*nF*nT + (type - 1)*nF*nT + (fdr - 1)*nT + t
+          far$Value[index] <- B_medians[1, t, cr, fdr]
+          far$Lower[index] <- B_lower[1, t, cr, fdr]
+          far$Upper[index] <- B_upper[1, t, cr, fdr]
+          near$Value[index] <- B_medians[2, t, cr, fdr]
+          near$Lower[index] <- B_lower[2, t, cr, fdr]
+          near$Upper[index] <- B_upper[2, t, cr, fdr]
+          inside$Value[index] <- B_medians[3, t, cr, fdr]
+          inside$Lower[index] <- B_lower[3, t, cr, fdr]
+          inside$Upper[index] <- B_upper[3, t, cr, fdr]
         }
       }
     }
   }
   
   ##### plotting parameters #####
-  y1 <- 0.35
-  y2 <- 1.4
-  y1.1 <- 0.35
-  y2.1 <- 1.4
-  # y1.1 <- 0.99
-  # y2.1 <- 1.75
-  jitter_height <- 0.00
+  jitter_height <- 0.005
   size1 <- 1.5
   size2 <- 0.75
   
-  ##### plot far #####
+  if (s != 4) {
+    colors <- c("#00BA38", "#00BFC4", "#619CFF", "#F564E3")
+  } else {
+    colors <- c("#F8766D", "#B79F00", "#00BA38", "#00BFC4", "#619CFF", "#F564E3")
+  }
   
-  # FAR <- ggplot(data = far, aes(x = Time, y = Value,
-  #                               color = as.factor(FDR),
-  #                               linetype = as.factor(Type))) +
-  #   geom_line(position = position_jitter(w = 0, h = jitter_height)) +
-  #   scale_linetype_manual(values = c(ltype1, ltype2)) +
-  #   geom_hline(yintercept = 1, linetype = 'dashed', color = 'black') +
-  #   labs(color = 'FDR', linetype = 'Type') +
-  #   ggtitle('Far From Reserve') +
-  #   ylab('Relative Biomass') +
-  #   theme(legend.position = 'none') +
-  #   theme(axis.title.x = element_blank()) +
-  #   ylim(y1, y2)
+  ##### plot outside (far) #####
   
   OUTSIDE <- ggplot(data = far, aes(x = Time, y = Value, 
                                     color = as.factor(FDR), 
                                     linetype = as.factor(Estimate), 
                                     size = as.factor(Type))) +
+    geom_ribbon(aes(ymin = Lower, ymax = Upper, fill = as.factor(FDR), colour = NA)) +
+    scale_fill_manual(values = alpha(c(colors), 0.05)) +
     geom_line(position = position_jitter(w = 0, h = jitter_height)) +
+    scale_color_manual(values = colors) +
     scale_size_manual(values = c(size1, size2)) +
     geom_hline(yintercept = 1, linetype = 'dashed', color = 'black') +
     labs(color = 'FDR', linetype = 'Estimate', size = 'Type') +
     ggtitle('Outside Reserve') +
     ylab('Relative Biomass') +
-    xlab('Years since reserve implementation') +
+    theme(axis.title.x = element_blank()) +
     theme(legend.position = 'none') +
     ylim(y1, y2)
-  
-  ##### plot near #####
-  
-  # NEAR <- ggplot(data = near, aes(x = Time, y = Value,
-  #                                 color = as.factor(FDR),
-  #                                 linetype = as.factor(Type))) +
-  #   geom_line(position = position_jitter(w = 0, h = jitter_height)) +
-  #   scale_linetype_manual(values = c(ltype1, ltype2)) +
-  #   geom_hline(yintercept = 1, linetype = 'dashed', color = 'black') +
-  #   labs(color = 'FDR', linetype = 'Type') +
-  #   ggtitle('Near Reserve') +
-  #   theme(axis.title.y = element_blank()) +
-  #   theme(axis.text.y = element_blank()) +
-  #   theme(legend.position = 'none')  +
-  #   ylim(y1, y2)
   
   ##### plot inside #####
   
   INSIDE <- ggplot(data = inside, aes(x = Time, y = Value,
                                       color = as.factor(FDR),
                                       linetype = as.factor(Estimate), 
-                                      size = as.factor(Type))) +
+                                      size = Type)) +
+    geom_ribbon(aes(ymin = Lower, ymax = Upper, fill = as.factor(FDR), 
+                    colour = NA), show.legend = FALSE) +
+    scale_fill_manual(values = alpha(colors, 0.05)) +
     geom_line(position = position_jitter(w = 0, h = jitter_height)) +
+    scale_color_manual(values = colors) +
     scale_size_manual(values = c(size1, size2)) +
     geom_hline(yintercept = 1, linetype = 'dashed', color = 'black') +
     ggtitle('Inside Reserve') +
     theme(axis.title.y = element_blank()) +
     xlab('Years since reserve implementation') +
+    theme(axis.title.x = element_text(hjust = 2.5)) +
     ylim(y1.1, y2.1) +
-    labs(color = 'FDR', linetype = 'Type')
+    labs(color = 'FDR', linetype = 'Estimate', size = 'Type') +
+    guides(color = guide_legend(order = 1), linetype = guide_legend(order = 2), 
+           size = guide_legend(order = 3))
   
   ##### patch all the figures together #####
   # patch <- (FAR + NEAR) / (INSIDE + plot_spacer())
@@ -203,8 +212,20 @@ for (s in 1:length(species_list)) {
   thing <- patch + plot_annotation(
     title = paste(Names[s], ': Relative Biomass', sep = ''))
   
-  ggsave(thing, filename = paste( 'M_biomass_relative', Names[s], '.png', sep = ''),
+  if (cluster == TRUE) {
+    
+    ggsave(thing, 
+           filename = paste( 'M_biomass_relative_', Names[s], '.png', sep = ''),
+           path = paste('~/Documents/MS-thesis/figures/', figures_folder, 
+                        sep = ''),
+           width = png_width, height = png_height)
+    
+  } else {
+    
+  ggsave(thing, filename = paste( 'M_biomass_relative_', Names[s], '.png', sep = ''),
          path = paste('C:/Users/Vic/Box/Quennessen_Thesis/figures/', 
-                      figures_folder, sep = ''))
+                      figures_folder, sep = ''),
+         width = png_width, height = png_height)
+  }
   
 }
