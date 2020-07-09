bfddfde7bb4319937bd3acfa288235f427ea3a28
Victoria Quennessen <viquennessen@gmail.com>
2020-07-08 18:44:09 -0700

    final machine runs

--- code/machine_runs.R
+++ code/machine_runs.R
@@ -4,7 +4,7 @@ library(densityratio)
 library(parallel)
 source('run_base_model.R')
 
-Scenario = 'Both.LowT'
+Scenario = 'Sampling.HighT'
 species_list_1 = c('CR_OR_2015', 'LING_OW_2017', 'BR_OR_2015')
 # species_list_1 = c('CR_OR_2015', 'BR_OR_2015')
 
@@ -13,8 +13,8 @@ species_list_2 = c('CAB_OR_2019')
 Final_DRs_1 <- c(0.6, 0.7, 0.8, 0.9)
 Final_DRs_2 <- c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 
-lapply(species_list_1, run_base_model, num_sims = 25, Scenario, Final_DRs_1)
-lapply(species_list_2, run_base_model, num_sims = 25, Scenario, Final_DRs_2)
+lapply(species_list_1, run_base_model, num_sims = 200, Scenario, Final_DRs_1)
+lapply(species_list_2, run_base_model, num_sims = 200, Scenario, Final_DRs_2)
 
 # lapply(species_list_1, run_base_model, num_sims = 10, Scenario, Final_DRs_1)
 # lapply(species_list_2, run_base_model, num_sims = 10, Scenario, Final_DRs_2)
