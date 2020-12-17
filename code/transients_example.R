b4d5130e1cf782bfd48728a42b8a5e04023b919f
Victoria Quennessen <viquennessen@gmail.com>
2020-12-17 08:31:02 -0800

    adjusted transients_example plot to not take error in M into account

--- code/transients_example.R
+++ code/transients_example.R
@@ -25,58 +25,93 @@ nT <- length(years)
 
 types <- c('Static', 'Transient')
 
-estimates <- c('Low', 'True', 'High')
-nE <- length(estimates)
+# estimates <- c('Low', 'True', 'High')
+# nE <- length(estimates)
 
 FDR <- 0.6
 
-DF <- data.frame(Species = rep(species_names, each = 2*nE*nT), 
-                 Type = rep(types, times = nS, each = nE*nT), 
-                 Estimate = rep(estimates, times = nS*2, each = nT),
-                 Time = rep(years, times = nS*2*nE), 
-                 Target = rep(0, nS*2*nE*nT))
+# DF <- data.frame(Species = rep(species_names, each = 2*nE*nT), 
+#                  Type = rep(types, times = nS, each = nE*nT), 
+#                  Estimate = rep(estimates, times = nS*2, each = nT),
+#                  Time = rep(years, times = nS*2*nE), 
+#                  Target = rep(0, nS*2*nE*nT))
+
+DF <- data.frame(Species = rep(species_names, each = 2*nT), 
+                 Type = rep(types, times = nS, each = nT), 
+                 Time = rep(years, times = nS*2), 
+                 Target = rep(0, nS*2*nT))
 
 for (s in 1:nS) {
   
   # Natural mortality values
   par <- parameters(species_list[s])
   M <- par[[2]]
-  Nat_mortalities <- c(M - 0.05, M, M + 0.05)
+  # Nat_mortalities <- c(M - 0.05, M, M + 0.05)
   
   # static control rules
-  static_start <- (s - 1)*2*nE*nT + 1
-  static_end <- static_start + nE*nT - 1
-  DF$Target[static_start:static_end] <- rep(FDR, nE*nT)
+  static_start <- (s - 1)*2*nT + 1
+  static_start
+  static_end <- static_start + nT - 1
+  static_end
+  DF$Target[static_start:static_end] <- rep(FDR, nT)
   
-  for (e in 1:nE) {
-
-    # natural mortality value
-    NM <- Nat_mortalities[e]
-
+  # for (e in 1:nE) {
+  # 
+  #   # natural mortality value
+  #   NM <- Nat_mortalities[e]
+  # 
+  #   # transient control rules
+  #   transient_start <- static_end + (e - 1)*nT  + 1
+  #   transient_stop <- transient_start + nT - 1
+  #   targets <- 1 - (1 - FDR)*(1 - exp(-1 * NM * years))
+  #   DF$Target[transient_start:transient_stop] <- targets
+  # 
+  # }
+    
     # transient control rules
-    transient_start <- static_end + (e - 1)*nT  + 1
+    transient_start <- static_end + 1
+    transient_start
     transient_stop <- transient_start + nT - 1
-    targets <- 1 - (1 - FDR)*(1 - exp(-1 * NM * years))
+    transient_stop
+    targets <- 1 - (1 - FDR)*(1 - exp(-1 * M * years))
     DF$Target[transient_start:transient_stop] <- targets
-
-  }
   
 }
 
-# make DF$Species is a factor variable
+# make DF$Species a factor variable
 DF$Species <- factor(DF$Species, levels = species_names)
-DF$Estimate <- factor(DF$Estimate, levels = estimates)
+# DF$Estimate <- factor(DF$Estimate, levels = estimates)
+
+# dfinal text
+dfinal <- expression('D'[final])
 
 # plot it
-example <- ggplot(data = DF, aes(x = Time, y = Target, linetype = Estimate, size = Type)) +
-  geom_line() +
-  scale_size_manual(values = c(2, 1)) +
-  ylab('Target Density Ratio') +
+# example <- ggplot(data = DF, aes(x = Time, y = Target, linetype = Estimate, 
+#                                  size = Type)) +
+#   geom_line() +
+#   scale_size_manual(values = c(2, 1)) +
+#   ylab('Target Density Ratio \n Dfinal value: 0.6') +
+#   xlab('Years Since Reserve Implementation') +
+#   scale_x_continuous(breaks = c(0, 10, 20)) +
+#   ylim(0.5, 1) +
+#   facet_grid(~ Species) +
+#   labs(size = 'Control Rule \n Type', 
+#        linetype = 'Estimate of \n Natural \n Mortality')
+
+example <- ggplot(data = DF, aes(x = Time, y = Target, color = Type,
+                                 linetype = Species)) +
+  geom_line(size = 1) +
+  ylab('Target Density Ratio \n Dfinal value: 0.6') +
   xlab('Years Since Reserve Implementation') +
-  scale_x_continuous(breaks = c(0, 10, 20)) +
-  ylim(0.5, 1) +
-  facet_grid(~ Species)
+  scale_linetype_manual(values = c('solid', 'dotdash', 'dotted', 'dashed')) +
+  scale_color_manual(values = c('grey50', 'black')) +
+  ylim(0.6, 1) +
+  theme_bw() +
+  scale_size(range = 1, guide = FALSE) +
+  labs(linetype = 'Species', color = 'Type of \n Control Rule') +
+  theme(legend.key.width = unit(1.5, 'cm')) +
+  guides(linetype = guide_legend(override.aes = list(size = 1)))
 
 ggsave(example, filename = 'transients_example.png', 
-       path = paste('C:/Users/Vic/Box/Quennessen_Thesis/figures/', sep = ''), 
-       width = 7, height = 2.5)
+       path = 'C:/Users/Vic/Box/Quennessen_Thesis/MS Thesis/publication manuscript/figures',
+       width = 7, height = 4)
