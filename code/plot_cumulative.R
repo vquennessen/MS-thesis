8a996651cd99858321fc7985f05c57956f9eddf2
Victoria Quennessen <viquennessen@gmail.com>
2020-12-21 11:54:34 -0800

    fixed script to produce cumulative mean and SD plots for mean biomass and yield at 5 and 20 years

--- code/plot_cumulative.R
+++ code/plot_cumulative.R
@@ -19,7 +19,7 @@ png_height <- 6
 
 # species to compare
 species_list <- c('CR_OR_2015', 'BR_OR_2015', 'LING_OW_2017', 'CAB_OR_2019')
-Names <- c('Canary Rockfish', 'Black Rockfish', 'Lingcod', 'Cabezon')
+Names <- c('Canary \n Rockfish', 'Black \n Rockfish', 'Lingcod', 'Cabezon')
 
 # determine num_sims based on data folder
 num_sims <- 3
@@ -49,18 +49,19 @@ nF1 <- length(Final_DRs1)
 nF2 <- length(Final_DRs2)
 nFt <- 3*nF1 + nF2
 
-DF1 <- data.frame(Species = c(rep(Names[1], each = nM*nTy*nF1*nY), 
-                              rep(Names[2], each = nM*nTy*nF1*nY), 
-                              rep(Names[3], each = nM*nTy*nF1*nY), 
-                              rep(Names[4], each = nM*nTy*nF2*nY)),
-                  Metric = c(rep(rep(metrics, each = nTy*nF1*nY), 3), 
-                             rep(metrics, each = nTy*nF2*nY)),
-                  Type = c(rep(rep(types, times = nM, each = nF1*nY), 3),
-                           rep(types, times = nM, each = nF2*nY)),
-                  FDR = c(rep(rep(Final_DRs1, times = nM*nTy, each = nY), 3),
-                          rep(Final_DRs2, times = nM*nTy, each = nY)),
-                  Year = rep(Years, times = nS*nM*nTy*nFt),
-                  Value = rep(NA, nS*nM*nTy*nFt*nY))
+DF <- data.frame(Species = c(rep(Names[1], each = nM*nTy*nF1*nY), 
+                             rep(Names[2], each = nM*nTy*nF1*nY), 
+                             rep(Names[3], each = nM*nTy*nF1*nY), 
+                             rep(Names[4], each = nM*nTy*nF2*nY)),
+                 Metric = c(rep(rep(metrics, each = nTy*nF1*nY), 3), 
+                            rep(metrics, each = nTy*nF2*nY)),
+                 Type = c(rep(rep(types, times = nM, each = nF1*nY), 3),
+                          rep(types, times = nM, each = nF2*nY)),
+                 FDR = c(rep(rep(Final_DRs1, times = nM*nTy, each = nY), 3),
+                         rep(Final_DRs2, times = nM*nTy, each = nY)),
+                 Year = rep(Years, times = nM*nTy*nFt),
+                 Mean = rep(NA, nM*nTy*nFt*nY), 
+                 SD = rep(NA, nM*nTy*nFt*nY))
 
 for (s in 1:length(Names)) {
   
@@ -103,89 +104,193 @@ for (s in 1:length(Names)) {
     for (fdr in 1:nF) {
       for (y in 1:nY) {
         
-        # biomass
+        # biomass index
+        index_b <- (s - 1)*nM*nTy*nF1*nY +  (ty - 1)*nF*nY + (fdr - 1)*nY + y
         
-        # yield
-      
+        # yield index
+        index_y <- index_b + nTy*nF*nY
+        
+        # initialize biomass and yield mean and SD vectors
+        bio_mean <- rep(0, num_sims)
+        bio_sd <- rep(0, num_sims)
+        
+        yield_mean <- rep(0, num_sims)
+        yield_sd <- rep(0, num_sims)
+        
+        # iterature through simulations to average values
+        for (sim in 1:num_sims) {
+          bio_mean[sim] <- mean(Rel_biomass[1:y, ty, fdr, sim]) 
+          bio_sd[sim] <- sd(Rel_biomass[1:y, ty, fdr, sim]) 
+          
+          yield_mean[sim] <- mean(Rel_yield[1:y, ty, fdr, sim]) 
+          yield_sd[sim] <- sd(Rel_yield[1:y, ty, fdr, sim]) 
+        }
+        
+        DF$Mean[index_b] <- median(bio_mean)
+        DF$SD[index_b]   <- median(bio_sd)
+        DF$Mean[index_y] <- median(yield_mean)
+        DF$SD[index_y]   <- median(yield_sd)
+        
       }
     }
   }
-  
-  # DF based on species
-  if (s == 4) { diff_DF <- diff2 } else { diff_DF <- diff1 }
-  
-  # make FDR and Estimate factor variables
-  DF$FDR <- factor(DF$FDR)
-  DF$Type.Metric <- paste(DF$Type, DF$Metric, sep = ' ')
-  DF$Type.Metric <- factor(DF$Type.Metric, 
-                           levels = c('Static Biomass', 'Static Yield', 
-                                      'Transient Biomass', 'Transient Yield'))
-  
-  # remove static biomass and yield for low and high estimates
-  new_DF <- subset(DF, Estimate == 'True' | Type == 'Transient')
-  
-  # panel labels
-  panel.labels <- c('(a) Biomass', '(b) Yield')
-  names(panel.labels) <- c('Biomass', 'Yield')
-  
-  # plotting parameters
-  og_colors <- rev(viridis(max(c(nF1, nF2)) + 1))
-  if (s != 4) {
-    new_colors <- og_colors[(nF2 - nF1 + 2):(nF2 + 1)]
-  } else {
-    new_colors <- og_colors[2:(nF2 + 1)]
-  }
-  
-  # plot relative results
-  thing1 <- ggplot(new_DF, aes(x = Estimate, y = Value, color = FDR, 
-                               shape = Type.Metric)) +
-    geom_hline(yintercept = 1, linetype = 2) +
-    geom_point(position = position_jitter(w = 0.25, h = 0), size = 3, stroke = 1) +
-    scale_shape_manual(values = c(16, 17, 1, 2), 
-                       labels = c('Static', 'Static', 'Transient', 'Transient')) +
-    scale_color_manual(values = new_colors, guide = FALSE) +
-    ylab('Median relative value') +
-    theme_bw() +
-    theme(axis.title.x = element_blank()) +
-    labs(shape = 'Type', color = expression('D'[final])) +
-    facet_grid(cols = vars(Metric), labeller = labeller(Metric = panel.labels))
-  
-  # make FDR and Estimate factor variables
-  diff_DF$FDR <- factor(diff_DF$FDR)
-  diff_DF$Estimate <- factor(diff_DF$Estimate, 
-                             levels = c('Low', 'True', 'High'))
-  
-  # panel labels
-  panel.labels <- c('(c) Biomass', '(d) Yield')
-  names(panel.labels) <- c('Biomass', 'Yield')
-  
-  # plot difference results
-  thing2 <- ggplot(diff_DF, aes(x = Estimate, y = Value, color = FDR, 
-                                shape = Metric)) +
-    geom_hline(yintercept = 0, linetype = 2) +
-    geom_point(position = position_jitter(w = 0.3, h = 0), size = 3, stroke = 1) +
-    scale_shape_manual(values = c(1, 2), guide = FALSE) +
-    scale_color_manual(values = new_colors) +
-    ylab('Median difference') +
-    xlab('Estimate of natural mortality (M)') +
-    theme_bw() +
-    labs(shape = 'Difference \n Metric', color = expression('D'[final])) +
-    guides(color = guide_legend(order = 1)) +
-    facet_grid(cols = vars(Metric), labeller = labeller(Metric = panel.labels))
-  
-  final_plot <- thing1 / thing2
-  
-  # save results to figures folder
-  if (cluster == TRUE) {
-    ggsave(final_plot, filename = paste(Names[s], '_year', Years[y], 
-                                        '_M.png', sep = ''),
-           path = paste('~/Documents/MS-thesis/figures/', folder, sep = ''),
-           width = png_width, height = png_height)
-    
-  } else {
-    ggsave(final_plot, filename = paste('M_', Names[s], '_year', Years[y], '.png', sep = ''),
-           path = 'C:/Users/Vic/Box/Quennessen_Thesis/publication manuscript/viridis figures/',
-           width = png_width, height = png_height)
-  }
-  
 }
+
+# make FDR and Metric factor variables
+DF$FDR <- factor(DF$FDR, levels = Final_DRs2)
+DF$Metric <- factor(DF$Metric, levels = metrics)
+DF$Type <- factor(DF$Type, levels = types)
+DF$Species <- factor(DF$Species, levels = Names)
+
+# remove static biomass and yield for low and high estimates
+years <- c(5, 20)
+bio_short <- subset(DF, Metric == 'Biomass' & Year == years[1])
+bio_long <- subset(DF, Metric == 'Biomass' & Year == years[2])
+
+yield_short <- subset(DF, Metric == 'Yield' & Year == years[1])
+yield_long <- subset(DF, Metric == 'Yield' & Year == years[2])
+
+# plotting parameters
+og_colors <- rev(viridis(max(c(nF1, nF2)) + 1))
+new_colors <- og_colors[2:(nF2 + 1)]
+
+# horizontal jitter amount
+w_jitter <- 0.3
+
+# legend position coordinates
+x.legend <- 1.25
+y.legend <- 1
+
+
+##### mean figure #####
+
+# plot cumulative mean biomass year 5
+thing1 <- ggplot(bio_short, aes(x = Species, y = Mean, color = FDR, 
+                                shape = Type)) +
+  geom_hline(yintercept = 1, linetype = 2) +
+  geom_point(position = position_jitter(w = w_jitter, h = 0), stroke = 1, 
+             size = 3) +
+  ggtitle('(a) Biomass: 5 years') +
+  scale_color_manual(values = new_colors) +
+  scale_shape_manual(values = c(1, 4)) +
+  ylab('relative biomass: mean') +
+  theme_bw() +
+  theme(axis.title.x = element_blank()) +
+  theme(legend.position = 'none')
+
+# plot cumulative mean yield year 5
+thing2 <- ggplot(yield_short, aes(x = Species, y = Mean, color = FDR, 
+                                  shape = Type)) +
+  geom_hline(yintercept = 1, linetype = 2) +
+  geom_point(position = position_jitter(w = w_jitter, h = 0), stroke = 1, 
+             size = 3) +
+  ggtitle('(c) Yield: 5 years') +
+  scale_color_manual(values = new_colors) +
+  scale_shape_manual(values = c(1, 4)) +
+  ylab('relative yield: mean') +
+  theme_bw() +
+  theme(axis.title.x = element_blank()) +
+  theme(legend.position = 'none')
+
+# plot cumulative mean biomass year 20
+thing3 <- ggplot(bio_long, aes(x = Species, y = Mean, color = FDR, 
+                               shape = Type)) +
+  geom_hline(yintercept = 1, linetype = 2) +
+  geom_point(position = position_jitter(w = w_jitter, h = 0), size = 3, 
+             stroke = 1) +
+  ggtitle('(b) Biomass: 20 years') +
+  scale_color_manual(values = new_colors) +
+  scale_shape_manual(values = c(1, 4)) +
+  theme_bw() +
+  theme(axis.title.x = element_blank()) +
+  theme(axis.title.y = element_blank()) +
+  theme(legend.position = 'none')
+
+# plot cumulative mean yield year 20
+thing4 <- ggplot(yield_long, aes(x = Species, y = Mean, color = FDR, 
+                                 shape = Type)) +
+  geom_hline(yintercept = 1, linetype = 2) +
+  geom_point(position = position_jitter(w = w_jitter, h = 0), size = 3, 
+             stroke = 1) +
+  ggtitle('(d) Yield: 20 years') +
+  scale_color_manual(values = new_colors) +
+  scale_shape_manual(values = c(1, 4)) +
+  theme_bw() +
+  theme(axis.title.x = element_blank()) +
+  theme(axis.title.y = element_blank()) +
+  labs(shape = 'Type of \n Control Rule', color = expression('D'[final])) +
+  theme(plot.margin = unit(c(0, 100, 0, 0), 'pt')) +
+  theme(legend.position = c(x.legend, y.legend))
+
+mean_plot <- (thing1 + thing3) / (thing2 + thing4)
+
+# save results to figures folder
+ggsave(mean_plot, filename = paste('cumulative_mean_plot.png', sep = ''),
+       path = paste('C:/Users/Vic/Box/Quennessen_Thesis/MS Thesis/', 
+                    'publication manuscript/viridis figures', sep = ''),
+       width = png_width, height = png_height)
+
+##### standard deviation figure #####
+
+# plot cumulative mean biomass year 5
+thing5 <- ggplot(bio_short, aes(x = Species, y = SD, color = FDR, 
+                                shape = Type)) +
+  geom_point(position = position_jitter(w = w_jitter, h = 0), stroke = 1, 
+             size = 3) +
+  ggtitle('(a) Biomass: 5 years') +
+  scale_color_manual(values = new_colors) +
+  scale_shape_manual(values = c(1, 4)) +
+  ylab('relative biomass: mean') +
+  theme_bw() +
+  theme(axis.title.x = element_blank()) +
+  theme(legend.position = 'none')
+
+# plot cumulative mean yield year 5
+thing6 <- ggplot(yield_short, aes(x = Species, y = SD, color = FDR, 
+                                  shape = Type)) +
+  geom_point(position = position_jitter(w = w_jitter, h = 0), stroke = 1, 
+             size = 3) +
+  ggtitle('(c) Yield: 5 years') +
+  scale_color_manual(values = new_colors) +
+  scale_shape_manual(values = c(1, 4)) +
+  ylab('relative yield: mean') +
+  theme_bw() +
+  theme(axis.title.x = element_blank()) +
+  theme(legend.position = 'none')
+
+# plot cumulative mean biomass year 20
+thing7 <- ggplot(bio_long, aes(x = Species, y = SD, color = FDR, 
+                               shape = Type)) +
+  geom_point(position = position_jitter(w = w_jitter, h = 0), size = 3, 
+             stroke = 1) +
+  ggtitle('(b) Biomass: 20 years') +
+  scale_color_manual(values = new_colors) +
+  scale_shape_manual(values = c(1, 4)) +
+  theme_bw() +
+  theme(axis.title.x = element_blank()) +
+  theme(axis.title.y = element_blank()) +
+  theme(legend.position = 'none')
+
+# plot cumulative mean yield year 20
+thing8 <- ggplot(yield_long, aes(x = Species, y = SD, color = FDR, 
+                                 shape = Type)) +
+  geom_point(position = position_jitter(w = w_jitter, h = 0), size = 3, 
+             stroke = 1) +
+  ggtitle('(d) Yield: 20 years') +
+  scale_color_manual(values = new_colors) +
+  scale_shape_manual(values = c(1, 4)) +
+  theme_bw() +
+  theme(axis.title.x = element_blank()) +
+  theme(axis.title.y = element_blank()) +
+  labs(shape = 'Type of \n Control Rule', color = expression('D'[final])) +
+  theme(plot.margin = unit(c(0, 100, 0, 0), 'pt')) +
+  theme(legend.position = c(x.legend, y.legend))
+
+sd_plot <- (thing5 + thing7) / (thing6 + thing8)
+
+# save results to figures folder
+ggsave(sd_plot, filename = paste('cumulative_sd_plot.png', sep = ''),
+       path = paste('C:/Users/Vic/Box/Quennessen_Thesis/MS Thesis/', 
+                    'publication manuscript/viridis figures', sep = ''),
+       width = png_width, height = png_height)
+
