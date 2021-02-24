53a881f0bb4d16dea2f0165b8c71beef93ca9267
Victoria Quennessen <viquennessen@gmail.com>
2021-02-24 13:36:00 -0800

    script to analysis proportions of control rules where transient CR led to higher biomass/yield/effort

---
+++ code/props_analysis.R
@@ -0,0 +1,50 @@
+# proportion analysis
+
+# set working directory
+setwd("~/Projects/MS-thesis/code")
+
+# load libraries
+library(dplyr)
+library(ggplot2)
+library(viridis)
+
+# load data
+data <- read.csv("proportion_transient_greater.csv", header = TRUE)
+
+# calculate means by grouping species by FDR value
+basic_FDR_year <- data %>%
+  group_by(Metric, Species, FDR) %>%
+  dplyr::summarize(Mean = mean(Value, na.rm=TRUE))
+
+basic_FDR_year
+
+# metrics
+metrics <- c('Biomass', 'Yield', 'Effort')
+metric_names <- c('Relative Biomass', 'Relative Yield', 'Relative Effort')
+basic_FDR_year$Metric <- factor(basic_FDR_year$Metric,levels = metrics, 
+                                labels = metric_names)
+
+# colors
+n <- length(unique(basic_FDR_year$Species))
+og_colors <- viridis(n + 1)
+colors <- og_colors[1:n]
+
+# figure width and height
+png_width <- 6
+# png_height <- 5
+
+# make plot
+fig <- ggplot(data = basic_FDR_year, 
+              aes(x = FDR, y = Mean, color = Species, shape = Species)) +
+  geom_hline(yintercept = 0.5, linetype = 'dotted', size = 0.75) +
+  geom_line(size = 0.75, alpha = 0.75) +
+  geom_point(size = 3, alpha = 0.75) +
+  scale_color_manual(values = colors) +
+  scale_shape_manual(values = c(15, 16, 17, 18)) + 
+  facet_grid(rows = vars(Metric), scales = 'free', switch = 'y') +
+  ylab('Mean Proportion of Runs') +
+  theme_bw()
+
+ggsave(fig, file = 'proportions_transient_greater.png', 
+       path = 'C:/Users/Vic/Box/Quennessen_Thesis/MS Thesis/publication manuscript/viridis figures',
+       width = png_width, heigh = png_height)
