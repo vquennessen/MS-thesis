50cb94c4f197b63114cbe8b14b84b2515d9fcd8b
Victoria Quennessen <viquennessen@gmail.com>
2021-07-18 08:58:51 -0700

    machine_runs

--- code/machine_runs.R
+++ code/machine_runs.R
@@ -11,22 +11,25 @@ species_list_2 = c('CAB_OR_2019')
 Final_DRs_1 <- c(0.6, 0.7, 0.8, 0.9)
 Final_DRs_2 <- c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 
-Scenario = 'None'
+NS1 <- 35
+NS2 <- 30
+NS3 <- 25
+NS4 <- 15
 
-lapply(species_list_1, run_base_model, num_sims = 1, Scenario, Final_DRs_1)
-lapply(species_list_2, run_base_model, num_sims = 1, Scenario, Final_DRs_2)
+Scenario = 'Recruitment'
 
-# Scenario = 'Recruitment'
-# 
-# lapply(species_list_1, run_base_model, num_sims = 1, Scenario, Final_DRs_1)
-# lapply(species_list_2, run_base_model, num_sims = 1, Scenario, Final_DRs_2)
-# 
+# lapply(species_list_1, run_base_model, num_sims = NS1, Scenario, Final_DRs_1)
+# lapply(species_list_2, run_base_model, num_sims = NS1, Scenario, Final_DRs_2)
+
 # Scenario = 'Sampling'
-# 
-# lapply(species_list_1, run_base_model, num_sims = 1, Scenario, Final_DRs_1)
-# lapply(species_list_2, run_base_model, num_sims = 1, Scenario, Final_DRs_2)
-# 
+
+lapply(species_list_1, run_base_model, num_sims = NS2, Scenario, Final_DRs_1)
+lapply(species_list_2, run_base_model, num_sims = NS2, Scenario, Final_DRs_2)
+
 # Scenario = 'Both'
-# 
-# lapply(species_list_1, run_base_model, num_sims = 1, Scenario, Final_DRs_1)
-# lapply(species_list_2, run_base_model, num_sims = 1, Scenario, Final_DRs_2)
+
+lapply(species_list_1, run_base_model, num_sims = NS3, Scenario, Final_DRs_1)
+lapply(species_list_2, run_base_model, num_sims = NS3, Scenario, Final_DRs_2)
+
+lapply(species_list_1, run_base_model, num_sims = NS4, Scenario, Final_DRs_1)
+lapply(species_list_2, run_base_model, num_sims = NS4, Scenario, Final_DRs_2)
