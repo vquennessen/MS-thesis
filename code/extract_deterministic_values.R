6eba1064aacfdec37de5b50df1e41ec8a277c827
vquennessen <viquennessen@gmail.com>
2021-10-23 16:57:32 -0700

    adjusted script to extract deterministic values for figures S11-14

--- code/extract_deterministic_values.R
+++ code/extract_deterministic_values.R
@@ -2,14 +2,14 @@
 
 # load any necessary libraries
 # library(plyr)
-remotes::install_github('vquennessen/densityratio')
-library(densityratio)
+# remotes::install_github('vquennessen/densityratio')
+# library(densityratio)
 
 # running on cluster or personal machine?
 cluster = TRUE
 
 # determine num_sims based on data folder
-num_sims <- 3
+num_sims <- 1
 
 # species to compare
 species_list <- c('CR_OR_2015', 'BR_OR_2015', 'LING_OW_2017', 'CAB_OR_2019')
@@ -24,7 +24,7 @@ Final_DRs1 <- c(0.6, 0.7, 0.8, 0.9)
 Final_DRs2 <- c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 Control_rules = c(1:6)
 types <- c('Static', 'Transient')
-metrics <- c('Relative Biomass', 'Relative Yield', 'Relative Effort')
+metrics <- c('Biomass', 'Yield', 'Effort')
 # , 'Density.Ratio')
 
 nY  <- Time2 + 1
@@ -46,36 +46,38 @@ DF <- data.frame(Species = c(rep(Names[1], each = nTy*nF1*nY),
                  FDR = c(rep(rep(Final_DRs1, times = nTy, each = nY), 3),
                          rep(Final_DRs2, times = nTy, each = nY)),
                  Year = rep(0:Time2, times = nTy*nFt),
-                 Value = rep(NA, nTy*nFt*nY))
+                 Value = rep(NA, nTy*nFt*nY), 
+                 Lower = rep(NA, nTy*nFt*nY), 
+                 Upper = rep(NA, nTy*nFt*nY))
 
 # copy base dataframes to metric specific ones
 BIOMASS <- DF
 YIELD   <- DF
 EFFORT  <- DF
-DR      <- DF
+# DR      <- DF
 
-for (s in 1:nS) {
+for (ss in 1:length(species_list)) {
   
   if (cluster == TRUE) {
     # load biomass, yield, and effort files
-    load(paste('~/Documents/MS-thesis/data/None/', species_list[s], 
-               '/3_biomass.Rda', sep = ''))
-    load(paste('~/Documents/MS-thesis/data/None/', species_list[s], 
-               '/3_yield.Rda', sep = ''))
-    load(paste('~/Documents/MS-thesis/data/None/', species_list[s], 
-               '/3_effort.Rda', sep = ''))
-    load(paste('~/Documents/MS-thesis/data/None/', species_list[s], 
-               '/3_DR.Rda', sep = ''))
+    load(paste('~/Documents/MS-thesis/data/None/', species_list[ss], 
+               '/1_biomass.Rda', sep = ''))
+    load(paste('~/Documents/MS-thesis/data/None/', species_list[ss], 
+               '/1_yield.Rda', sep = ''))
+    load(paste('~/Documents/MS-thesis/data/None/', species_list[ss], 
+               '/1_effort.Rda', sep = ''))
+    # load(paste('~/Documents/MS-thesis/data/None/', species_list[ss], 
+    #            '/1_DR.Rda', sep = ''))
   } else {
     # load biomass, yield, and effort files
-    load(paste('~/Projects/MS-thesis/data/None/', species_list[s], 
-               '/3_biomass.Rda', sep = ''))
-    load(paste('~/Projects/MS-thesis/data/None/', species_list[s], 
-               '/3_yield.Rda', sep = ''))
-    load(paste('~/Projects/MS-thesis/data/None/', species_list[s], 
-               '/3_effort.Rda', sep = ''))
-    load(paste('~/Projects/MS-thesis/data/None/', species_list[s], 
-               '/3_DR.Rda', sep = ''))
+    load(paste('~/Projects/MS-thesis/data/None/', species_list[ss], 
+               '/1_biomass.Rda', sep = ''))
+    load(paste('~/Projects/MS-thesis/data/None/', species_list[ss], 
+               '/1_yield.Rda', sep = ''))
+    load(paste('~/Projects/MS-thesis/data/None/', species_list[ss], 
+               '/1_effort.Rda', sep = ''))
+    # load(paste('~/Projects/MS-thesis/data/None/', species_list[ss], 
+    #            '/1_DR.Rda', sep = ''))
   }
   
   
@@ -83,28 +85,23 @@ for (s in 1:nS) {
   B_sample  <- colSums(sims_biomass[ , , 1:2, , ]) 
   Y_sample  <- sims_yield[ , 1:2, , ]
   E_sample  <- sims_effort[ , 1:2, , ]
-  DR_sample <- sims_DR[ , 1:2, , ]
+  # DR_sample <- sims_DR[ , 1:2, , ]
   
   # set nF based on species
-  nF <- ifelse(s == 4, nF2, nF1)  
+  nF <- ifelse(ss == 4, nF2, nF1)  
   
   # initialize relative arrays
-  Rel_biomass <- array(rep(0, nY*nTy*nF*num_sims), c(nY, nTy, nF, num_sims))
-  Rel_yield   <- array(rep(0, nY*nTy*nF*num_sims), c(nY, nTy, nF, num_sims))
-  Rel_effort  <- array(rep(0, nY*nTy*nF*num_sims), c(nY, nTy, nF, num_sims))
+  Rel_biomass <- array(rep(0, nY*nTy*nF), c(nY, nTy, nF))
+  Rel_yield   <- array(rep(0, nY*nTy*nF), c(nY, nTy, nF))
+  Rel_effort  <- array(rep(0, nY*nTy*nF), c(nY, nTy, nF))
   
   # calculate relative arrays after reserve implementation
   for (ty in 1:nTy) {
     for (fdr in 1:nF) {
-      for (sim in 1:num_sims) {
-        Rel_biomass[, ty, fdr, sim] <- B_sample[, ty, fdr, sim] / 
-          B_sample[1, ty, fdr, sim]
-        Rel_yield[, ty, fdr, sim] <- Y_sample[, ty, fdr, sim] / 
-          Y_sample[1, ty, fdr, sim]
-        Rel_effort[, ty, fdr, sim] <- E_sample[Time1:(Time1 + Time2), 
-                                               ty, fdr, sim] / 
-          E_sample[1, ty, fdr, sim]
-      }
+        Rel_biomass[, ty, fdr] <- B_sample[1:nY, ty, fdr] / B_sample[1, ty, fdr]
+        Rel_yield[, ty, fdr] <- Y_sample[1:nY, ty, fdr] / Y_sample[1, ty, fdr]
+        Rel_effort[, ty, fdr] <- E_sample[Time1:(Time1 + Time2), ty, fdr] / 
+          E_sample[1, ty, fdr]
     }
   }
   
@@ -113,19 +110,27 @@ for (s in 1:nS) {
     for (fdr in 1:nF) {
       for (y in 1:nY) {
         
-        index <- (s-1)*nTy*nF1*nY + (ty - 1)*nF*nY + (fdr - 1)*nY + y
+        index <- (ss-1)*nTy*nF1*nY + (ty - 1)*nF*nY + (fdr - 1)*nY + y
         
         # relative biomass
-        BIOMASS$Value[index] <- median(Rel_biomass[y, ty, fdr, ])
+        BIOMASS$Value[index] <- Rel_biomass[y, ty, fdr]
+        BIOMASS$Lower[index] <- Rel_biomass[y, ty, fdr]
+        BIOMASS$Upper[index] <- Rel_biomass[y, ty, fdr]
         
         # relative yield
-        YIELD$Value[index] <- median(Rel_yield[y, ty, fdr, ])
+        YIELD$Value[index] <- Rel_yield[y, ty, fdr]
+        YIELD$Lower[index] <- Rel_yield[y, ty, fdr]
+        YIELD$Upper[index] <- Rel_yield[y, ty, fdr]
         
         # relative effort
-        EFFORT$Value[index] <- median(Rel_effort[y, ty, fdr, ])
+        EFFORT$Value[index] <- Rel_effort[y, ty, fdr]
+        EFFORT$Lower[index] <- Rel_effort[y, ty, fdr]
+        EFFORT$Upper[index] <- Rel_effort[y, ty, fdr]
         
         # true density ratio
-        DR$Value[index] <- median(DR_sample[y, ty, fdr, ])
+        # DR$Value[index] <- DR_sample[y, ty, fdr]
+        # DR$Lower[index] <- DR_sample[y, ty, fdr]
+        # DR$Upper[index] <- DR_sample[y, ty, fdr]
         
       }
     }
@@ -139,15 +144,13 @@ for (s in 1:nS) {
 BIOMASS$Metric <- 'Relative Biomass'
 YIELD$Metric   <- 'Relative Yield'
 EFFORT$Metric  <- 'Relative Effort'
-# DR$Metric <- 'Density Ratio'
+# DR$Metric      <- 'Density Ratio'
 
 # combine to a single dataframe
 # dataframe <- rbind(BIOMASS, YIELD, EFFORT, DR)
 DF <- rbind(BIOMASS, YIELD, EFFORT)
 
 # add Source column
-DF$Lower <- NA
-DF$Upper <- NA
 DF$Source <- 'Deterministic'
 
 # add in scenarios
@@ -161,7 +164,7 @@ DF_both$Scenario <- 'Both'
 # merge dataframes again
 dataframe <- rbind(DF_rec, DF_samp, DF_both)
 
-# rearrange columns to match plot_sampling_panels dataframe
+# rearrange columns to match plot_stochastic dataframe
 dataframe <- dataframe[, c('Species', 'Scenario', 'Metric', 'Type', 'FDR', 
                            'Year', 'Value', 'Lower', 'Upper', 'Source')]
 
