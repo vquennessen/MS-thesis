ebfad59e1695d9b7d986dcad6d079b9b5ed8404b
vquennessen <viquennessen@gmail.com>
2022-03-17 13:07:39 -0700

    created script for outputting data from overfished BR runs

---
+++ code/overfishing_runs.R
@@ -0,0 +1,26 @@
+# deterministic runs for overfished scenario (historical FM = 0.23)
+
+# set working directory
+setwd('C:/Users/Vic/Documents/Projects/MS-thesis/code')
+
+# load libraries
+library(remotes)
+remotes::install_github('vquennessen/densityratio')
+library(densityratio)
+library(parallel)
+source('run_base_model.R')
+
+# species to run
+species_list_1 = c('BR_OR_2015_overfished')
+
+# final density ratios
+Final_DRs_1 <- c(0.6, 0.7, 0.8, 0.9)
+
+# stochastic scenario
+Scenario = 'None'
+
+# number of runs
+NS1 = 1
+
+# run simulations
+lapply(species_list_1, run_base_model, num_sims = NS1, Scenario, Final_DRs_1)
