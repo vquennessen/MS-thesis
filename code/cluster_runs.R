8b8e55961d80ce51db736b670830b14e3204bd6c
vquennessen <viquennessen@gmail.com>
2022-03-22 16:57:55 -0700

    edited cluster_runs.R to start running not overfishing BR_OR_2015 runs

--- code/cluster_runs.R
+++ code/cluster_runs.R
@@ -8,26 +8,28 @@ source('run_base_model.R')
 # species_list_2 = c('CAB_OR_2019')
 # Final_DRs_2 <- c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 
-species_list_1 = c('CR_OR_2015')
+species_list_1 <- c('BR_OR_2015')
 Final_DRs_1 <- c(0.6, 0.7, 0.8, 0.9)
+
+NS1 <- 100
 
 ########### Recruitment ########################################################
 
 Scenario = 'Recruitment'
 
-mclapply(species_list_1, run_base_model, mc.cores = 12, num_sims = 100,
+mclapply(species_list_1, run_base_model, mc.cores = 10, num_sims = NS1,
          Scenario, Final_DRs_1)
 
 ########### Sampling ###########################################################
 
 Scenario = 'Sampling'
 
-mclapply(species_list_1, run_base_model, mc.cores = 12, num_sims = 100,
+mclapply(species_list_1, run_base_model, mc.cores = 10, num_sims = NS1,
          Scenario, Final_DRs_1)
 
 ######### Both #################################################################
 
 Scenario = 'Both'
 
-mclapply(species_list_1, run_base_model, mc.cores = 12, num_sims = 100,
+mclapply(species_list_1, run_base_model, mc.cores = 10, num_sims = NS1,
          Scenario, Final_DRs_1)
