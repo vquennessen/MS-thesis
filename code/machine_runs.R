f268ff21f1938f197bbfe058d225c2544cb27079
vquennessen <viquennessen@gmail.com>
2022-04-01 16:08:59 -0700

    adjusted machine runs for deterministic runs for all species

--- code/machine_runs.R
+++ code/machine_runs.R
@@ -11,21 +11,21 @@ species_list_2 = c('CAB_OR_2019')
 Final_DRs_1 <- c(0.6, 0.7, 0.8, 0.9)
 Final_DRs_2 <- c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 
-NS1 <- 40
-NS2 <- 25
-NS3 <- 20
-NS4 <- 15
+NS1 <- 1
+# NS2 <- 25
+# NS3 <- 20
+# NS4 <- 15
 
-Scenario = 'Both'
+Scenario = 'None'
 
-# lapply(species_list_1, run_base_model, num_sims = NS1, Scenario, Final_DRs_1)
+lapply(species_list_1, run_base_model, num_sims = NS1, Scenario, Final_DRs_1)
 lapply(species_list_2, run_base_model, num_sims = NS1, Scenario, Final_DRs_2)
 
-lapply(species_list_1, run_base_model, num_sims = NS2, Scenario, Final_DRs_1)
-lapply(species_list_2, run_base_model, num_sims = NS2, Scenario, Final_DRs_2)
-
-lapply(species_list_1, run_base_model, num_sims = NS3, Scenario, Final_DRs_1)
-lapply(species_list_2, run_base_model, num_sims = NS3, Scenario, Final_DRs_2)
-
-lapply(species_list_1, run_base_model, num_sims = NS4, Scenario, Final_DRs_1)
-lapply(species_list_2, run_base_model, num_sims = NS4, Scenario, Final_DRs_2)
+# lapply(species_list_1, run_base_model, num_sims = NS2, Scenario, Final_DRs_1)
+# lapply(species_list_2, run_base_model, num_sims = NS2, Scenario, Final_DRs_2)
+# 
+# lapply(species_list_1, run_base_model, num_sims = NS3, Scenario, Final_DRs_1)
+# lapply(species_list_2, run_base_model, num_sims = NS3, Scenario, Final_DRs_2)
+# 
+# lapply(species_list_1, run_base_model, num_sims = NS4, Scenario, Final_DRs_1)
+# lapply(species_list_2, run_base_model, num_sims = NS4, Scenario, Final_DRs_2)
