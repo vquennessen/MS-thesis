d47ef94a3683413c042ea24546f4820ef152e89a
vquennessen <viquennessen@gmail.com>
2022-04-01 16:04:19 -0700

    asjusted cluster_runs.R for new set of runs

--- code/cluster_runs.R
+++ code/cluster_runs.R
@@ -8,28 +8,38 @@ source('run_base_model.R')
 # species_list_2 = c('CAB_OR_2019')
 # Final_DRs_2 <- c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 
-species_list_1 <- c('BR_OR_2015')
+species_list_1 <- c('LING_OW_2017', 'CR_OR_2015')
+species_list_2 = c('CAB_OR_2019')
+
 Final_DRs_1 <- c(0.6, 0.7, 0.8, 0.9)
+Final_DRs_2 <- c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 
 NS1 <- 100
+mc <- 10
 
 ########### Recruitment ########################################################
 
 Scenario = 'Recruitment'
 
-mclapply(species_list_1, run_base_model, mc.cores = 10, num_sims = NS1,
+mclapply(species_list_1, run_base_model, mc.cores = mc, num_sims = NS1,
          Scenario, Final_DRs_1)
+mclapply(species_list_2, run_base_model, mc.cores = mc, num_sims = NS1,
+         Scenario, Final_DRs_2)
 
 ########### Sampling ###########################################################
 
 Scenario = 'Sampling'
 
-mclapply(species_list_1, run_base_model, mc.cores = 10, num_sims = NS1,
+mclapply(species_list_1, run_base_model, mc.cores = mc, num_sims = NS1,
          Scenario, Final_DRs_1)
+mclapply(species_list_2, run_base_model, mc.cores = mc, num_sims = NS1,
+         Scenario, Final_DRs_2)
 
 ######### Both #################################################################
 
 Scenario = 'Both'
 
-mclapply(species_list_1, run_base_model, mc.cores = 10, num_sims = NS1,
+mclapply(species_list_1, run_base_model, mc.cores = mc, num_sims = NS1,
          Scenario, Final_DRs_1)
+mclapply(species_list_2, run_base_model, mc.cores = mc, num_sims = NS1,
+         Scenario, Final_DRs_2)
